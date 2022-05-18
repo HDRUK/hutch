26d13d23587603e9b4414a4cb904b3bdc792f5cc
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-05-18 12:27:53 +0100

    docstring for `BaseDBManager`

--- app/LinkLiteAgent/linkliteagent/db_manager.py
+++ app/LinkLiteAgent/linkliteagent/db_manager.py
@@ -1,15 +1,32 @@
+from typing import Any
+
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.engine import URL
 
 
 class BaseDBManager:
-    drivername = None
-
     def __init__(
-        self, username: str, password: str, host: str, port: int, database: str
+        self,
+        username: str,
+        password: str,
+        host: str,
+        port: int,
+        database: str,
+        drivername: str,
     ) -> None:
+        """Constructor method for DBManager classes.
+        Creates the connection engine and the inpector for the database.
+
+        Args:
+            username (str): The username for the database.
+            password (str): The password for the database.
+            host (str): The host for the database.
+            port (int): The port number for the database.
+            database (str): The name of the database.
+            drivername (str): The database driver e.g. "psycopg2", "pymysql", etc.
+        """
         url = URL(
-            drivername=self.drivername,
+            drivername=drivername,
             username=username,
             password=password,
             host=host,
@@ -19,7 +36,12 @@ class BaseDBManager:
         self.engine = create_engine(url=url)
         self.inspector = inspect(self.engine)
 
-    def exectute(self, stmnt) -> None:
+    def exectute(self, stmnt: Any) -> None:
+        """Execute a statement against the database.
+
+        Args:
+            stmnt (Any): The statement to be executed.
+        """
         pass
 
     def list_tables(self) -> list:
