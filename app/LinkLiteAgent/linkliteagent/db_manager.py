91d9534592071f7b937f3ef3a2e3a84106bb9f29
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-05-18 13:50:18 +0100

    synchronous db manager

--- app/LinkLiteAgent/linkliteagent/db_manager.py
+++ app/LinkLiteAgent/linkliteagent/db_manager.py
@@ -36,13 +36,39 @@ class BaseDBManager:
         self.engine = create_engine(url=url)
         self.inspector = inspect(self.engine)
 
-    def exectute(self, stmnt: Any) -> None:
-        """Execute a statement against the database.
+    def exectute_and_fetch(self, stmnt: Any) -> list:
+        """Execute a statement against the database and fetch the result.
 
         Args:
-            stmnt (Any): The statement to be executed.
+            stmnt (Any): The statement object to be executed.
+
+        Returns:
+            list: The list of rows returned.
+        """
+        pass
+
+    def execute(self, stmnt: Any) -> None:
+        """Execute a statement against the database and don't fetch any results.
+
+        Args:
+            stmnt (Any): The statement object to be executed.
         """
         pass
 
     def list_tables(self) -> list:
         pass
+
+
+class SyncDBManager(BaseDBManager):
+    def exectute_and_fetch(self, stmnt: Any) -> list:
+        with self.engine.begin() as conn:
+            result = conn.execute(statement=stmnt)
+            rows = result.all()
+        return rows
+
+    def execute(self, stmnt) -> None:
+        with self.engine.begin() as conn:
+            conn.execute(statement=stmnt)
+
+    def list_tables(self) -> list:
+        return self.inspector.get_table_names()
