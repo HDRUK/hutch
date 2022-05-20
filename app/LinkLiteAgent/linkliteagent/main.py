99e08506153673efc327ecd46c5d0c1ab9f622df
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-05-20 16:47:23 +0100

    configure logging in `main.py`

--- app/LinkLiteAgent/linkliteagent/main.py
+++ app/LinkLiteAgent/linkliteagent/main.py
@@ -2,32 +2,56 @@ import sys
 import os
 import logging
 import time
-import asyncio
+import pika
+from linkliteagent.db_manager import SyncDBManager
+from linkliteagent.db_logging import SyncLogDBHandler
 
 
-async def loop():
-    """The main method"""
-    running = True
-    # send the logging to stdout
-    handler = logging.StreamHandler(sys.stdout)
-    # set up the logger
-    logger = logging.getLogger("linkliteagent")
-    logger.setLevel(logging.INFO)
-    logger.addHandler(handler)
-
-    while running:
-        try:
-            # hello world
-            logger.info(f"Hello World @ {time.ctime()}")
-            time.sleep(5)
-        except KeyboardInterrupt:
-            # shut down on Ctrl+C
-            logger.warning(f"{os.linesep}Shut down @ {time.ctime()}")
-            running = False
+async def async_main():
+    """An asynchronous version of the main function"""
+    pass
 
 
 def main():
-    asyncio.run(loop())
+    """The main method"""
+    format = logging.Formatter(
+        "%(levelname)s - %(asctime)s - %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+    )
+
+    # set up the backup logger
+    backup_handler = logging.StreamHandler(sys.stdout)
+    backup_handler.setFormatter(format)
+    backup_logger = logging.getLogger("backup_logger")
+    backup_logger.setLevel(logging.INFO)
+    backup_logger.addHandler(backup_handler)
+
+    # set up the db logger
+    db_manager = SyncDBManager(
+        drivername="postgresql+psycopg2",
+        username="postgres",
+        password="example",
+        host="localhost",
+        port=5432,
+        database="postgres",
+    )
+    db_handler = SyncLogDBHandler(db_manager, "backup_logger")
+    db_handler.setFormatter(format)
+    db_logger = logging.getLogger("db_logger")
+    db_logger.setLevel(logging.INFO)
+    db_logger.addHandler(db_handler)
+    db_logger.addHandler(backup_handler)
+
+    running = True
+    while running:
+        try:
+            # hello world
+            db_logger.info(f"Hello World @ {time.ctime()}")
+            time.sleep(2)
+        except KeyboardInterrupt:
+            # shut down on Ctrl+C
+            db_logger.warning(f"{os.linesep}Shut down @ {time.ctime()}")
+            running = False
 
 
 if __name__ == "__main__":
