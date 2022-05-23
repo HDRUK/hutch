e6083372e4999c6ced478a05c5815c57dc62881e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-05-23 11:25:12 +0100

    place holder for query callback

---
+++ app/LinkLiteAgent/linkliteagent/query.py
@@ -0,0 +1,19 @@
+from pika.channel import Channel
+from pika.spec import Basic, BasicProperties
+
+
+def query_callback(
+    channel: Channel, method: Basic.Deliver, properties: BasicProperties, body: bytes
+):
+    """The callback to be used when consuming messages from the queue.
+    The arguments to this function will be passed by the channel when a
+    message is consumed.
+
+    Args:
+        channel (Channel): The channel object.
+        method (Deliver): The delivery object.
+        properties (BasicProperties): The message properties.
+        body (bytes): The body of the message.
+    """
+    # TODO: implement the behaviour in future PR.
+    pass
