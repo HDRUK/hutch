5907a6936ce5bde4135f78e8dfab76dbe557b6c0
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-05-30 14:46:51 +0100

    add config values to `main.py` and `query.py`

--- app/LinkLiteAgent/linkliteagent/config.py
+++ app/LinkLiteAgent/linkliteagent/config.py
@@ -2,13 +2,15 @@
 DB_LOGGER_NAME = "db"
 BACKUP_LOGGER_NAME = "backup"
 LOGS_AND_CONFIG_DB = {
-    "drivername": "postgressql",
+    "drivername": "postgresql+psycopg2",
     "username": "postgres",
     "password": "example",
     "database": "postgres",
     "host": "localhost",
     "port": 5432,
 }
+MSG_FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
+DATE_FORMAT = "%d-%b-%y %H:%M:%S"
 
 # RabbitMQ configuration
 QUEUE_NAME = "jobs"
