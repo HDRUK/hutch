884a8bcce7a112f06cb90e69db2a24547c3689b8
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-06-09 11:46:27 +0100

    measurement table representation

--- app/LinkLiteAgent/linkliteagent/entities.py
+++ app/LinkLiteAgent/linkliteagent/entities.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy import BigInteger, Column, Date, ForeignKey, Integer, Numeric, String, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -23,3 +23,29 @@ class Person(Base):
     race_source_concept_id = Column(Integer, nullable=True)
     ethnicity_source_value = Column(String(50), nullable=True)
     ethnicity_source_concept_id = Column(Integer, nullable=True)
+
+class Measurement(Base):
+    __tablename__ = "measurement"
+    measurement_id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, ForeignKey("person.person_id"), nullable=False)
+    measurement_concept_id = Column(Integer, nullable=False)
+    measurement_date = Column(Date, nullable=False)
+    measurement_datetime = Column(DateTime, nullable=True)
+    measurement_time = Column(String(10), nullable=True)
+    measurement_type_concept_id = Column(Integer, nullable=False)
+    operator_concept_id = Column(Integer, nullable=True)
+    value_as_number = Column(Numeric, nullable=True)
+    value_as_concept_id = Column(Integer, nullable=True)
+    unit_concept_id = Column(Integer, nullable=True)
+    range_low = Column(Numeric, nullable=True)
+    range_high = Column(Numeric, nullable=True)
+    provider_id = Column(Integer, nullable=True)
+    visit_occurrence_id = Column(Integer, nullable=True)
+    visit_detail_id = Column(Integer, nullable=True)
+    measurement_source_value = Column(String(50), nullable=True)
+    measurement_source_concept_id = Column(Integer, nullable=True)
+    unit_source_value = Column(String(50), nullable=True)
+    unit_source_concept_id = Column(Integer, nullable=True)
+    value_source_value = Column(String(50), nullable=True)
+    measurement_event_id = Column(BigInteger, nullable=True)
+    meas_event_field_concept_id = Column(Integer, nullable=True)
