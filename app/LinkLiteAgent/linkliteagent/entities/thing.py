ece1b97b6eef161f2afc806ffb95aedef25a7d75
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-06-15 14:33:46 +0100

    factory method on `Thing`

--- app/LinkLiteAgent/linkliteagent/entities/thing.py
+++ app/LinkLiteAgent/linkliteagent/entities/thing.py
@@ -1,12 +1,13 @@
 import json
+from typing_extensions import Self
 
 
 class Thing:
     """Python representation of [Thing](https://schema.org/Thing)."""
 
-    def __init__(self, context: str, type: str, name: str, **kwargs) -> None:
+    def __init__(self, context: str, type_: str, name: str, **kwargs) -> None:
         self.context = context
-        self.type = type
+        self.type_ = type_
         self.name = name
 
     def to_dict(self) -> dict:
@@ -17,9 +18,17 @@ class Thing:
         """
         return {
             "@context": self.context,
-            "@type": self.name,
+            "@type": self.type_,
             "name": self.name,
         }
+
+    @classmethod
+    def from_dict(cls, dict_: dict) -> Self:
+        return cls(
+            context=dict_.get("@context", ""),
+            type_=dict_.get("@type", ""),
+            name=dict_.get("name", ""),
+        )
 
     def __str__(self) -> str:
         """`Thing` as a JSON string.
