e703c0dce40f1cf805efc4c489a57ddf3c371c82
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2022-06-15 10:53:53 +0100

    Remove old tables from db context

--- app/LinkLiteManager/Data/ApplicationDbContext.cs
+++ app/LinkLiteManager/Data/ApplicationDbContext.cs
@@ -20,28 +20,9 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 
   public DbSet<Logs> Logs => Set<Logs>();
 
-  public DbSet<Person> Person => Set<Person>();
-
-  public DbSet<ConditionOccurrence> ConditionOccurrence => Set<ConditionOccurrence>();
-  public DbSet<Measurement> Measurement => Set<Measurement>();
-  public DbSet<Observation> Observation => Set<Observation>();
-
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
     base.OnModelCreating(modelBuilder);
-    modelBuilder.Entity<Person>()
-      .HasMany(p => p.ConditionOccurrences)
-      .WithOne(co => co.Person)
-      .HasForeignKey("PersonId");
-    modelBuilder.Entity<Person>()
-      .HasMany(p => p.Observations)
-      .WithOne(o => o.Person)
-      .HasForeignKey("PersonId");
-    modelBuilder.Entity<Person>()
-      .HasMany(p => p.Measurements)
-      .WithOne(m => m.Person)
-      .HasForeignKey("PersonId");
-
   }
 
 }
