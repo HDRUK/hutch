526cb9bf19ed92a88ddf38d89af292610d3abb99
Samad198 <samad198@live.co.uk>
2022-06-17 09:25:11 +0100

    AB#62653: Activity source list (#17)
    * added activity source list to home page ui
    
    * replaced simple grid with VStack

--- app/manager-frontend/src/pages/UserHome.jsx
+++ app/manager-frontend/src/pages/UserHome.jsx
@@ -1,32 +1,35 @@
-import { Heading, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Heading, SimpleGrid, VStack, Text } from "@chakra-ui/react";
 import { useUser } from "contexts/User";
 import { useTranslation } from "react-i18next";
+import { ActionCard } from "components/ActionCard";
+import { useSortingAndFiltering } from "helpers/hooks/useSortingAndFiltering";
+import { useActivitySourceList } from "api/activitysource";
+import { ActivitySourceSummary } from "components/ActivitySourceSummary";
+//import { useEffect } from "react";
 
 export const UserHome = () => {
   const { user } = useUser();
   const { t } = useTranslation();
+  const { data } = useActivitySourceList();
+
   return (
     <VStack align="stretch" px={8} w="100%" spacing={4} p={4}>
       <Heading as="h2" size="lg">
         {t("home.heading", { name: user?.fullName })}
       </Heading>
-
-      <SimpleGrid minChildWidth="400px" spacing={4}>
-        {/* <ActionCard
-          href="process-overview"
-          icon={AiFillRead}
-          title="Proposals process overview"
-        >
-          <Text>
-            Review the process for submitting proposals using this system
-          </Text>
-        </ActionCard> */}
-      </SimpleGrid>
-
       <VStack w="100%" align="stretch">
         <Heading as="h3" size="lg">
-          Collections
+          Activity Sources
         </Heading>
+        {data.map((item, index) => (
+          <ActivitySourceSummary
+            key={index}
+            href=""
+            title={item.DisplayName}
+            sourceURL={item.Host}
+            collectionId={item.ResourceId}
+          ></ActivitySourceSummary>
+        ))}
       </VStack>
     </VStack>
   );
