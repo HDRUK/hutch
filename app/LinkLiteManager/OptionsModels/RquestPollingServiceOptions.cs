712983d2fd8eb07d9c49b6a59dd2d91e27f34c52
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2022-06-20 10:54:37 +0100

    Remove RquestCollectionId from model

--- app/LinkLiteManager/OptionsModels/RquestPollingServiceOptions.cs
+++ app/LinkLiteManager/OptionsModels/RquestPollingServiceOptions.cs
@@ -7,10 +7,5 @@
         /// for fetching queries from the RQUEST Connector API
         /// </summary>
         public int QueryPollingInterval { get; set; } = 5;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public string RquestCollectionId { get; set; } = "";
     }
 }
