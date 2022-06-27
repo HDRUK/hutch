192acee3ed182407a9fbfb82d610b367cfa3f624
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-06-27 15:12:52 +0100

    re-write query class and add missing objects

--- app/HutchAgent/hutchagent/ro_crates/query.py
+++ app/HutchAgent/hutchagent/ro_crates/query.py
@@ -10,13 +10,27 @@ class Query:
 
     def __init__(
         self,
-        graph: List[Union[Group, Operator]],
+        groups: List[Group],
         group_operator: Operator,
+        collection: str,
+        uuid: str,
+        char_salt: str,
+        task_id: str,
+        project: str,
+        owner: str,
+        protocol_version: str,
         context: str = "https://w3id.org/ro/crate/1.1/context",
     ) -> None:
         self.context = context
-        self.graph = [] if graph is None else graph
+        self.groups = [] if groups is None else groups
         self.group_operator = group_operator
+        self.collection = collection
+        self.uuid = uuid
+        self.char_salt = char_salt
+        self.task_id = task_id
+        self.project = project
+        self.owner = owner
+        self.protocol_version = protocol_version
 
     def to_dict(self) -> dict:
         """Convert `Query` to `dict`.
@@ -26,8 +40,52 @@ class Query:
         """
         return {
             "@context": self.context,
-            "@graph": [self.group_operator.to_dict()]
-            + [thing.to_dict() for thing in self.graph],
+            "@graph": [
+                {
+                    "@context": "https://schema.org",
+                    "@type": "PropertyValue",
+                    "name": "collection",
+                    "value": self.collection,
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "PropertyValue",
+                    "name": "uuid",
+                    "value": self.uuid,
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "PropertyValue",
+                    "name": "char_salt",
+                    "value": self.char_salt,
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "PropertyValue",
+                    "name": "task_id",
+                    "value": self.task_id,
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "PropertyValue",
+                    "name": "project",
+                    "value": self.project,
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "PropertyValue",
+                    "name": "owner",
+                    "value": self.owner,
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "PropertyValue",
+                    "name": "protocol_version",
+                    "value": self.protocol_version,
+                },
+                self.group_operator.to_dict(),
+            ]
+            + [thing.to_dict() for thing in self.groups],
         }
 
     @classmethod
@@ -40,16 +98,47 @@ class Query:
         Returns:
             Self: `Query` object.
         """
+        collection = ""
+        uuid = ""
+        task_id = ""
+        char_salt = ""
+        project = ""
+        owner = ""
+        protocol_version = ""
         graph_list = dict_.get("@graph", [])
-        graph = []
+        groups = []
         group_operator = None
         for g in graph_list:
             if g.get("name") == "groupOperator":
                 group_operator = Operator.from_dict(g)
+            elif g.get("name") == "collection":
+                collection = g.get("value")
+            elif g.get("name") == "uuid":
+                uuid = g.get("value")
+            elif g.get("name") == "task_id":
+                task_id = g.get("value")
+            elif g.get("name") == "char_salt":
+                char_salt = g.get("value")
+            elif g.get("name") == "project":
+                project = g.get("value")
+            elif g.get("name") == "owner":
+                owner = g.get("value")
+            elif g.get("name") == "protocol_version":
+                protocol_version = g.get("value")
             else:
-                graph.append(Group.from_dict(g))
+                groups.append(Group.from_dict(g))
 
-        return cls(graph=graph, group_operator=group_operator)
+        return cls(
+            groups=groups,
+            group_operator=group_operator,
+            collection=collection,
+            uuid=uuid,
+            task_id=task_id,
+            char_salt=char_salt,
+            project=project,
+            owner=owner,
+            protocol_version=protocol_version,
+        )
 
     def __str__(self) -> str:
         return json.dumps(self.to_dict(), indent=2)
--- app/HutchAgent/tests/test_ro_crates.py
+++ app/HutchAgent/tests/test_ro_crates.py
@@ -4,6 +4,61 @@ from hutchagent.ro_crates.operator import Operator
 from hutchagent.ro_crates.query import Query
 from hutchagent.ro_crates.rule import Rule
 
+PROJECT_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyValue",
+    "name": "project",
+    "value": "fake_project",
+}
+
+OWNER_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyValue",
+    "name": "owner",
+    "value": "fake_user",
+}
+
+COLLECTION_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyValue",
+    "name": "collection",
+    "value": "collection id",
+}
+
+UUID_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyValue",
+    "name": "uuid",
+    "value": "not-a-real-uuid",
+}
+
+CHAR_SALT_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyValue",
+    "name": "char_salt",
+    "value": "fake char salt",
+}
+
+TASK_ID_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyValue",
+    "name": "task_id",
+    "value": "fake task id",
+}
+
+PROTOCOL_VERSION_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyValue",
+    "name": "protocol_version",
+    "value": "v2",
+}
+
+GROUP_OPERATOR_DICT = {
+    "@context": "https://schema.org",
+    "@type": "PropertyVale",
+    "name": "groupOperator",
+    "value": "AND",
+}
 
 RULE_DICT = {
     "@context": "https://schema.org",
@@ -40,12 +95,14 @@ GROUP_DICT = {
 QUERY_DICT = {
     "@context": "https://w3id.org/ro/crate/1.1/context",
     "@graph": [
-        {
-            "@context": "https://schema.org",
-            "@type": "PropertyVale",
-            "name": "groupOperator",
-            "value": "AND",
-        },
+        COLLECTION_DICT,
+        UUID_DICT,
+        CHAR_SALT_DICT,
+        TASK_ID_DICT,
+        PROJECT_DICT,
+        OWNER_DICT,
+        PROTOCOL_VERSION_DICT,
+        GROUP_OPERATOR_DICT,
         GROUP_DICT,
     ],
 }
@@ -68,7 +125,14 @@ def test_group():
 
 def test_query():
     query = Query.from_dict(QUERY_DICT)
-    for g in query.graph:
+    for g in query.groups:
         assert isinstance(g, Group)
     assert isinstance(query.group_operator, Operator)
+    assert query.project == PROJECT_DICT.get("value")
+    assert query.collection == COLLECTION_DICT.get("value")
+    assert query.char_salt == CHAR_SALT_DICT.get("value")
+    assert query.owner == OWNER_DICT.get("value")
+    assert query.protocol_version == PROTOCOL_VERSION_DICT.get("value")
+    assert query.uuid == UUID_DICT.get("value")
+    assert query.task_id == TASK_ID_DICT.get("value")
     assert query.to_dict() == QUERY_DICT
