d05f2bf9bdc24e060118fe25d39803095ecb78ba
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2022-07-05 11:10:57 +0100

    Add PropertyValues

--- app/HutchManager/Services/QueryTranslator.cs
+++ app/HutchManager/Services/QueryTranslator.cs
@@ -1,3 +1,4 @@
+using HutchManager.Data.Entities;
 using HutchManager.Dto;
 
 namespace HutchManager.Services;
@@ -8,20 +9,33 @@ public class QueryTranslator
   { 
     ROCratesQuery Translate(T input);
   }
-  public class RquestQueryTranslator: IQueryTranslator<RquestQuery>{
-    public ROCratesQuery Translate(RquestQuery rquestQuery)
+  public class RquestQueryTranslator: IQueryTranslator<RquestQueryTask>{
+    public ROCratesQuery Translate(RquestQueryTask job)
     {
       
       ROCratesQuery roCratesQuery = new();
       var Graph = new ROCratesQuery().Graphs;
+      Graph.Add(new ROCratesQuery.ROCratesGraph());
+      //Add ActivitySourceID
+      Graph[0].Context = "https://schema.org";
+      Graph[0].Type = "PropertyValue";
+      Graph[0].Name = "activity_source_id";
+      Graph[0].Value = job.ActivitySourceId;
+      
+      // Add Job ID
+      Graph[1].Context = "https://schema.org";
+      Graph[1].Type = "PropertyValue";
+      Graph[1].Name = "activity_source_id";
+      Graph[1].Value = job.JobId;
+      
       var item = new List<ROCratesQuery.Item>();
       
       roCratesQuery.Context = "https://w3id.org/ro/crate/1.1/context";
-      int length = rquestQuery.Groups.Count();
+      int length = job.Query.Groups.Count()+2;
       //For each group in an rquestQuery create a Graph in roCratesQuery
-      for (int i=0;i<length;i++)
+      for (int i=2;i<length;i++)
       {
-        var ruleLength = rquestQuery.Groups[i].Rules.Count();
+        var ruleLength = job.Query.Groups[i].Rules.Count();
         Graph.Add(new ROCratesQuery.ROCratesGraph());
         Graph[i].Context = "https://schema.org";
         Graph[i].Type = "ItemList";
@@ -31,7 +45,7 @@ public class QueryTranslator
       //For each rule in a query create an Item in a Graph
         for (int j = 0; j < ruleLength; j++)
         {
-          var rule = rquestQuery.Groups[i].Rules[j];
+          var rule = job.Query.Groups[i].Rules[j];
           
           item.Add(new ROCratesQuery.Item());
           item[j].Context = "https://schema.org";
