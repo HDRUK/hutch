b87122c68914c9cf7f0d83be531b7ca13ff58010
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2022-07-05 10:47:28 +0100

    Modify for statement to match rocrates schema

--- app/HutchManager/Services/QueryTranslator.cs
+++ app/HutchManager/Services/QueryTranslator.cs
@@ -11,39 +11,45 @@ public class QueryTranslator
   public class RquestQueryTranslator: IQueryTranslator<RquestQuery>{
     public ROCratesQuery Translate(RquestQuery rquestQuery)
     {
+      
       ROCratesQuery roCratesQuery = new();
+      var Graph = new ROCratesQuery().Graphs;
+      var item = new List<ROCratesQuery.Item>();
       
       roCratesQuery.Context = "https://w3id.org/ro/crate/1.1/context";
       int length = rquestQuery.Groups.Count();
+      //For each group in an rquestQuery create a Graph in roCratesQuery
       for (int i=0;i<length;i++)
       {
         var ruleLength = rquestQuery.Groups[i].Rules.Count();
+        Graph.Add(new ROCratesQuery.ROCratesGraph());
+        Graph[i].Context = "https://schema.org";
+        Graph[i].Type = "ItemList";
+        Graph[i].Type = "group";
+        Graph[i].NumberOfItems = ruleLength;
+       
+      //For each rule in a query create an Item in a Graph
         for (int j = 0; j < ruleLength; j++)
         {
           var rule = rquestQuery.Groups[i].Rules[j];
-          Console.WriteLine(rule.Value);
-
-          var initialGraph = new ROCratesQuery.ROCratesGraph();
-          initialGraph.Context = "https://schema.org";
-          initialGraph.Type = "ItemList";
-          initialGraph.Type = "group";
-          initialGraph.NumberOfItems = ruleLength;
           
-          var item = new ROCratesQuery.Item();
-          item.Context = "https://schema.org";
-          item.Type = rule.Type;
-          item.Value = rule.Value;
-
-          var properties = new ROCratesQuery.Property();
+          item.Add(new ROCratesQuery.Item());
+          item[j].Context = "https://schema.org";
+          item[j].Type = rule.Type;
+          item[j].Value = rule.Value;
           
-          properties.Context = "https://schema.org";
-          properties.Type = rule.Type;
-          properties.Name = "operator";
-          properties.Value = rule.Operand;
+          var properties = new List<ROCratesQuery.Property>();
+          properties.Add(new ROCratesQuery.Property());
+          properties[j].Context = "https://schema.org";
+          properties[j].Type = rule.Type;
+          properties[j].Name = "operator";
+          properties[j].Value = rule.Operand;
           
-          roCratesQuery.Graphs[i]= initialGraph;
-          roCratesQuery.Graphs[i].ItemListElements[j] = item;
-          roCratesQuery.Graphs[i].ItemListElements[j].AdditionalProperties[j] = properties;
+          item[j].AdditionalProperties = properties;
+          Graph[i].ItemListElements = item;
+          roCratesQuery.Graphs = Graph;
+          
+          
         }
       }
       return roCratesQuery;
