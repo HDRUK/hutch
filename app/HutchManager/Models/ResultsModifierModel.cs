1f15b2b28675c533b836955badac98335fe6fa46
Samad198 <eeysaad@nottingham.ac.uk>
2022-07-11 13:02:01 +0100

    AB#65592:Migrations (#52)
    Added migrations for results modifier

--- app/HutchManager/Models/ResultsModifierModel.cs
+++ app/HutchManager/Models/ResultsModifierModel.cs
@@ -1,4 +1,4 @@
-using System.Text.Json.Nodes;
+using System.Text.Json;
 
 namespace HutchManager.Models;
 
@@ -8,8 +8,7 @@ public class ResultsModifierModel
   public int Order { get; set; }
   public ActivitySourceModel ActivitySource { get; set; } = new();
   public ModifierTypeModel Type { get; set; } = new();
-
-  public JsonObject Parameters { get; set; } = new();
+  public JsonDocument? Parameters { get; set; }
 
   public ResultsModifierModel(Data.Entities.ResultsModifier entity)
   {
