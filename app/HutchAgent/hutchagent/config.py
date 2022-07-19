cbc914edb6c3e8ae81bfb1fd3300e4d65379ebc9
Jon Couldridge <jcouldridge@outlook.com>
2022-07-19 12:30:34 +0100

    moving some config to a central internal file

---
+++ app/HutchAgent/hutchagent/config.py
@@ -0,0 +1,8 @@
+DEFAULT_DB_DRIVER="postgresql"
+
+# Logging configuration
+DB_LOGGER_NAME="db"
+BACKUP_LOGGER_NAME="backup"
+MSG_FORMAT="%(levelname)s - %(asctime)s - %(message)s"
+DATE_FORMAT="%d-%b-%y %H:%M:%S"
+
