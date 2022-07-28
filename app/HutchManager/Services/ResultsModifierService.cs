34b04caf13239744c6371af922afd2f123b1d1bf
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2022-07-28 14:18:26 +0100

    Format code

--- app/HutchManager/Services/ResultsModifierService.cs
+++ app/HutchManager/Services/ResultsModifierService.cs
@@ -1,6 +1,4 @@
 using HutchManager.Data;
-using HutchManager.Data.Entities;
-using HutchManager.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace HutchManager.Services;
@@ -8,6 +6,7 @@ namespace HutchManager.Services;
 public class ResultsModifierService
 {
   private readonly ApplicationDbContext _db;
+
   public ResultsModifierService(ApplicationDbContext db)
   {
     _db = db;
@@ -19,9 +18,9 @@ public class ResultsModifierService
       .AsNoTracking()
       .Include(x => x.Type)
       .Include(x => x.ActivitySource)
-        .ThenInclude(x => x.Type)
+      .ThenInclude(x => x.Type)
       .Include(x => x.ActivitySource)
-        .ThenInclude(x => x.TargetDataSource)
+      .ThenInclude(x => x.TargetDataSource)
       .ToListAsync();
     return list.ConvertAll<Models.ResultsModifierModel>(x => new(x));
   }
@@ -36,22 +35,23 @@ public class ResultsModifierService
 
   public async Task<Models.ResultsModifierModel> Create(Models.CreateResultsModifier resultsModifier)
   {
-
-
     var type = (await _db.ModifierTypes.ToListAsync()).FirstOrDefault(x => x.Id == resultsModifier.Type) ??
-       throw new InvalidOperationException($"Type {resultsModifier.Type} is not a valid ModifierType");
-    var activitySource = (await _db.ActivitySources.ToListAsync()).FirstOrDefault(x => x.Id == resultsModifier.ActivitySourceId) ??
-       throw new InvalidOperationException($"Activity Source {resultsModifier.ActivitySourceId} is not a valid Activity Source");
- 
-    var limitCheck = (await _db.ResultsModifier.ToListAsync())
-      .Where(x => x.ActivitySource.Id == activitySource.Id && x.Type.Id == type.Id );
+               throw new InvalidOperationException($"Type {resultsModifier.Type} is not a valid ModifierType");
+    var activitySource =
+      (await _db.ActivitySources.ToListAsync()).FirstOrDefault(x => x.Id == resultsModifier.ActivitySourceId) ??
+      throw new InvalidOperationException(
+        $"Activity Source {resultsModifier.ActivitySourceId} is not a valid Activity Source");
 
-    if(limitCheck.Count() >= type.Limit)
+    var limitCheck = (await _db.ResultsModifier.ToListAsync())
+      .Where(x => x.ActivitySource.Id == activitySource.Id && x.Type.Id == type.Id);
+
+    if (limitCheck.Count() >= type.Limit)
     {
-      throw new BadHttpRequestException($"Cannot create any more modifiers of type {type.Id} for the activity source {activitySource.DisplayName}");
+      throw new BadHttpRequestException(
+        $"Cannot create any more modifiers of type {type.Id} for the activity source {activitySource.DisplayName}");
     }
 
-    var entity = resultsModifier.ToEntity(type,activitySource);
+    var entity = resultsModifier.ToEntity(type, activitySource);
     await _db.ResultsModifier.AddAsync(entity);
     await _db.SaveChangesAsync();
     return new(entity);
@@ -66,11 +66,13 @@ public class ResultsModifierService
     if (entity is null)
       throw new KeyNotFoundException(
         $"No ResultsModifier with ID: {id}");
-    
+
     var type = (await _db.ModifierTypes.ToListAsync()).FirstOrDefault(x => x.Id == resultsModifier.Type) ??
-       throw new InvalidOperationException($"Type {resultsModifier.Type} is not a valid ModifierType");
-    var activitySource = (await _db.ActivitySources.ToListAsync()).FirstOrDefault(x => x.Id == resultsModifier.ActivitySourceId) ??
-       throw new InvalidOperationException($"Activity Source {resultsModifier.ActivitySourceId} is not a valid Activity Source");
+               throw new InvalidOperationException($"Type {resultsModifier.Type} is not a valid ModifierType");
+    var activitySource =
+      (await _db.ActivitySources.ToListAsync()).FirstOrDefault(x => x.Id == resultsModifier.ActivitySourceId) ??
+      throw new InvalidOperationException(
+        $"Activity Source {resultsModifier.ActivitySourceId} is not a valid Activity Source");
     entity.Order = resultsModifier.Order;
     entity.Type = type;
     entity.Parameters = resultsModifier.Parameters;
@@ -78,29 +80,30 @@ public class ResultsModifierService
     await _db.SaveChangesAsync();
     return new(entity);
   }
+
   public async Task<Models.ResultsModifierModel> Get(int resultsModifierId)
   {
     var resultsModifier = await _db.ResultsModifier
-                     .AsNoTracking()
-                     .Include(x => x.Type)
-                     .Include(x=>x.ActivitySource.Type)
-                     .Include(x=>x.ActivitySource.TargetDataSource)
-                     .Where(x => x.Id == resultsModifierId)
-                     .SingleOrDefaultAsync()
-                   ?? throw new KeyNotFoundException();
+                            .AsNoTracking()
+                            .Include(x => x.Type)
+                            .Include(x => x.ActivitySource.Type)
+                            .Include(x => x.ActivitySource.TargetDataSource)
+                            .Where(x => x.Id == resultsModifierId)
+                            .SingleOrDefaultAsync()
+                          ?? throw new KeyNotFoundException();
     return new(resultsModifier);
   }
-  
+
   public async Task<List<Models.ResultsModifierModel>> GetActivitySourceResultsModifier(int activitySourceId)
   {
     var resultsModifierList = await _db.ResultsModifier
-                            .AsNoTracking()
-                            .Include(x => x.Type)
-                            .Include(x=>x.ActivitySource)
-                            .Include(x=> x.ActivitySource.Type)
-                            .Include(x => x.ActivitySource.TargetDataSource)
-                            .Where(x => x.ActivitySource.Id == activitySourceId)
-                            .ToListAsync();
+      .AsNoTracking()
+      .Include(x => x.Type)
+      .Include(x => x.ActivitySource)
+      .Include(x => x.ActivitySource.Type)
+      .Include(x => x.ActivitySource.TargetDataSource)
+      .Where(x => x.ActivitySource.Id == activitySourceId)
+      .ToListAsync();
     return resultsModifierList.ConvertAll<Models.ResultsModifierModel>(x => new(x));
   }
 
@@ -116,12 +119,5 @@ public class ResultsModifierService
         $"No Results Modifier with ID: {resultsModifierId}");
     _db.ResultsModifier.Remove(entity);
     await _db.SaveChangesAsync();
-
   }
-
 }
-
-
-
-
-
