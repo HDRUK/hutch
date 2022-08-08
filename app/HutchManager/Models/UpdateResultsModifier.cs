ed736f5b82bb2828048433bff1809e329f9d30ee
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2022-08-08 14:59:57 +0100

    Split Update/Create models

---
+++ app/HutchManager/Models/UpdateResultsModifier.cs
@@ -0,0 +1,20 @@
+using System.Text.Json;
+
+namespace HutchManager.Models;
+
+public class UpdateResultsModifier
+{
+  public int Order { get; set; }
+  public int ActivitySourceId { get; set; }
+  public string Type { get; set; } = string.Empty;
+  public JsonDocument? Parameters { get; set; }
+
+  public Data.Entities.ResultsModifier ToEntity(Data.Entities.ModifierType type, Data.Entities.ActivitySource source)
+    => new()
+    {
+      Order = Order,
+      Type = type,
+      Parameters = Parameters,
+      ActivitySource = source
+    };
+}
