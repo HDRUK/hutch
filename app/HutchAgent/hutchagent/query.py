5f653d7163633fe8d8ad5b921e7353d7bed18267
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-08-24 13:39:39 +0100

    add changes to rquest

--- app/HutchAgent/hutchagent/query.py
+++ app/HutchAgent/hutchagent/query.py
@@ -489,10 +489,11 @@ class RQuestQueryBuilder(BaseQueryBuilder):
         group_stmnt = self.subqueries[0].alias("group_0")
         for i, sq in enumerate(self.subqueries[1:]):
             sq = sq.alias(f"group_{i + 1}")
+            join_cols = (group_stmnt.c.keys()[0], sq.c.keys()[0])
             group_stmnt = group_stmnt.join(
                 sq,
-                group_stmnt.c.keys()[0] == sq.c.keys()[0],
-                full=self.query.group_operator.value == "OR"
+                group_stmnt.c[join_cols[0]] == sq.c[join_cols[1]],
+                full=self.query.cohort.groups_oper == "OR"
             )
         self.subqueries.clear()
         stmnt = select(func.count()).select_from(group_stmnt)
