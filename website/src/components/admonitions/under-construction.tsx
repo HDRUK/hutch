cda3d231d108121f6f497ed3a857905f677d37bb
Jon Couldridge <jcouldridge@outlook.com>
2022-10-05 12:23:26 +0100

    agent installation docs are no longer incorrect; just incomplete

---
+++ website/src/components/admonitions/under-construction.tsx
@@ -0,0 +1,19 @@
+/**
+ * 
+  :::caution 🚧 Under Construction
+  This section is a work in progress. It may be incomplete and is subject to change.
+  :::
+ * 
+ */
+
+import React from "react";
+import Admonition from "@theme/Admonition";
+
+export const SectionUnderConstruction = () => (
+  <Admonition type="caution" title="🚧 Under Construction">
+    <p>
+      This section is a work in progress. It may be incomplete and is subject to
+      change.
+    </p>
+  </Admonition>
+);
