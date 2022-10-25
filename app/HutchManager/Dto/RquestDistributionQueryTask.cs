59d1f96bfccd1f21eb8ed14570cf49217152319a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-10-25 13:35:18 +0100

    getters and setters for `RquestDistributionQueryTask.cs`

--- app/HutchManager/Dto/RquestDistributionQueryTask.cs
+++ app/HutchManager/Dto/RquestDistributionQueryTask.cs
@@ -9,31 +9,31 @@ public class RquestDistributionQueryTask
   /// The user that creates the task.
   /// </summary>
   [JsonPropertyName("owner")]
-  public string Owner = String.Empty;
+  public string Owner { get; set; } = String.Empty;
 
   /// <summary>
   /// The code for the type of distribution query.
   /// Possible values: "GENERIC", "DEMOGRAPHICS" or "ICD-MAN"
   /// </summary>
   [JsonPropertyName("code")]
-  public string Code = String.Empty;
+  public string Code  { get; set; } = String.Empty;
 
   /// <summary>
   /// The type of analysis to be carried out.
   /// Possible values: "DISTRIBUTION"
   /// </summary>
   [JsonPropertyName("analysis")]
-  public string Analysis = "DISTRIBUTION";
-  
+  public string Analysis { get; set; } = "DISTRIBUTION";
+
   /// <summary>
   /// The unique identifier for the distribution query.
   /// </summary>
   [JsonPropertyName("uuid")]
-  public string Uuid = String.Empty;
-  
+  public string Uuid { get; set; } = String.Empty;
+
   /// <summary>
   /// The collection ID to run the query against.
   /// </summary>
   [JsonPropertyName("collection")]
-  public string Collection = String.Empty;
+  public string Collection { get; set; } = String.Empty;
 }
