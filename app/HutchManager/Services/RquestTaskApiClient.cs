49fece2651620df5f33f2018c33d2c4a16b53657
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-11-14 13:32:57 +0000

    Remove redundant body checks

--- app/HutchManager/Services/RquestTaskApiClient.cs
+++ app/HutchManager/Services/RquestTaskApiClient.cs
@@ -12,193 +12,188 @@ using System.Text.Json;
 using HutchManager.Constants;
 
 
-namespace HutchManager.Services
+namespace HutchManager.Services;
+
+public class RQuestTaskApiClient
 {
-  public class RQuestTaskApiClient
+  private readonly HttpClient _client;
+  private readonly ILogger<RQuestTaskApiClient> _logger;
+  private readonly RQuestTaskApiOptions _apiOptions;
+  private readonly ApplicationDbContext _db;
+
+  public RQuestTaskApiClient(
+    HttpClient client,
+    ILogger<RQuestTaskApiClient> logger,
+    IOptions<RQuestTaskApiOptions> apiOptions,
+    ApplicationDbContext db)
   {
-    private readonly HttpClient _client;
-    private readonly ILogger<RQuestTaskApiClient> _logger;
-    private readonly RQuestTaskApiOptions _apiOptions;
-    private readonly ApplicationDbContext _db;
+    _client = client;
+    _logger = logger;
+    _apiOptions = apiOptions.Value;
+    _db = db;
 
-    public RQuestTaskApiClient(
-      HttpClient client,
-      ILogger<RQuestTaskApiClient> logger,
-      IOptions<RQuestTaskApiOptions> apiOptions,
-      ApplicationDbContext db)
+    // TODO: credentials in future will be per Activity Source, so won't be set as default
+    string credentials = _apiOptions.Username + ":" + _apiOptions.Password;
+    var authString = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
+    _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authString);
+  }
+
+  /// <summary>
+  /// Serialize a value to a JSON string, and provide HTTP StringContent
+  /// for it with a media type of "application/json"
+  /// </summary>
+  /// <param name="value"></param>
+  /// <returns>HTTP StringContent with the value serialized to JSON and a media type of "application/json"</returns>
+  private StringContent AsHttpJsonString<T>(T value)
+    => new StringContent(
+      JsonSerializer.Serialize(value),
+      Encoding.UTF8,
+      "application/json");
+
+  /// <summary>
+  /// Try and get a job for a collection
+  /// </summary>
+  /// <param name="activitySource"> ActivitySource</param>
+  /// <returns>A Task DTO containing a Query to run, or null if none are waiting</returns>
+  public async Task<T?> FetchQuery<T>(ActivitySource activitySource) where T : class, IRquestTask, new()
+  {
+    var typeSuffix = new T() switch
     {
-      _client = client;
-      _logger = logger;
-      _apiOptions = apiOptions.Value;
-      _db = db;
+      RquestQueryTask _ => RQuestJobTypeSuffixes.AvailabilityQuery,
+      RquestDistributionQueryTask _ => RQuestJobTypeSuffixes.Distribution,
+      _ => string.Empty
 
-      // TODO: credentials in future will be per Activity Source, so won't be set as default
-      string credentials = _apiOptions.Username + ":" + _apiOptions.Password;
-      var authString = Convert.ToBase64String(Encoding.UTF8.GetBytes(credentials));
-      _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authString);
-    }
+    };
+    var requestUri = Url.Combine(
+      activitySource.Host,
+      _apiOptions.EndpointBase,
+      _apiOptions.FetchQueryEndpoint,
+      // Currently this method only looks for "Availability Queries""
+      activitySource.ResourceId + typeSuffix);
+    var result = await _client.GetAsync(
+      requestUri);
 
-    /// <summary>
-    /// Serialize a value to a JSON string, and provide HTTP StringContent
-    /// for it with a media type of "application/json"
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns>HTTP StringContent with the value serialized to JSON and a media type of "application/json"</returns>
-    private StringContent AsHttpJsonString<T>(T value)
-      => new StringContent(
-        JsonSerializer.Serialize(value),
-        Encoding.UTF8,
-        "application/json");
-
-    /// <summary>
-    /// Try and get a job for a collection
-    /// </summary>
-    /// <param name="activitySource"> ActivitySource</param>
-    /// <returns>A Task DTO containing a Query to run, or null if none are waiting</returns>
-    public async Task<T?> FetchQuery<T>(ActivitySource activitySource) where T: class, IRquestTask, new()
+    if (result.IsSuccessStatusCode)
     {
-      var typeSuffix = new T() switch
+      if (result.StatusCode == HttpStatusCode.NoContent)
       {
-        RquestQueryTask _ => RQuestJobTypeSuffixes.AvailabilityQuery,
-        RquestDistributionQueryTask _ => RQuestJobTypeSuffixes.Distribution,
-        _ => string.Empty
-        
-      };
-      var requestUri = Url.Combine(
-        activitySource.Host,
-        _apiOptions.EndpointBase,
-        _apiOptions.FetchQueryEndpoint,
-        // Currently this method only looks for "Availability Queries""
-        activitySource.ResourceId + typeSuffix);
-      var result = await _client.GetAsync(
-        requestUri);
-
-      if (result.IsSuccessStatusCode)
-      {
-        if (result.StatusCode == HttpStatusCode.NoContent)
-        {
-          _logger.LogInformation(
-            "No Query Jobs waiting for {ResourceId}",
-            activitySource.ResourceId);
-          return null;
-        }
-
-        try
-        {
-          var job = await result.Content.ReadFromJsonAsync<T>();
-
-          // a null job is impossible because the necessary JSON payload
-          // to achieve it would fail deserialization
-          _logger.LogInformation("Found Query with Id: {JobId}", job!.JobId);
-          //Set ActivitySource ID
-          job.ActivitySourceId = activitySource.Id;
-          return job;
-        }
-        catch (JsonException e)
-        {
-          _logger.LogError(e, "Invalid Response Format from Fetch Query Endpoint");
-
-          var body = await result.Content.ReadAsStringAsync();
-          _logger.LogDebug("Invalid Response Body: {Body}", body);
-
-          throw;
-        }
-      }
-      else
-      {
-        _logger.LogError("Fetch Query Endpoint Request failed: {StatusCode}", result.StatusCode);
-        throw new ApplicationException($"Fetch Query Endpoint Request failed: {result.StatusCode}");
-      }
-    }
-
-    /// <summary>
-    /// Post to the Results endpoint, and handle the response correctly
-    /// </summary>
-    /// <param name="activitySourceId">activitySourceId ID</param>
-    /// <param name="jobId">Job ID</param>
-    /// <param name="result">Results with Count</param>
-    public async Task ResultsEndpointPost(int activitySourceId, string jobId, QueryResultCount result)
-    {
-      var activitySource = await _db.ActivitySources
-        .FirstOrDefaultAsync(x => x.Id == activitySourceId);
-
-      if (activitySource is null)
-        throw new KeyNotFoundException(
-          $"No ActivitySource with ID: {activitySourceId}");
-
-      var requestUri = Url.Combine(
-        activitySource.Host,
-        _apiOptions.EndpointBase,
-        _apiOptions.SubmitResultEndpoint,
-        jobId,
-        activitySource.ResourceId);
-
-      var response = (await _client.PostAsync(
-          requestUri, AsHttpJsonString(new RquestQueryTaskResult(activitySource.ResourceId, jobId, result.Count))))
-        .EnsureSuccessStatusCode();
-
-      var body = await response.Content.ReadAsStringAsync();
-
-      if (body != "Job saved" && !response.IsSuccessStatusCode)
-      {
-        const string message = "Unsuccessful Response from Submit Results Endpoint";
-        _logger.LogError(message);
-        _logger.LogDebug("Response Body: {Body}", body);
-
-        throw new ApplicationException(message);
+        _logger.LogInformation(
+          "No Query Jobs waiting for {ResourceId}",
+          activitySource.ResourceId);
+        return null;
       }
 
-      return;
-    }
+      try
+      {
+        var job = await result.Content.ReadFromJsonAsync<T>();
 
-    public async Task DistributionResultsEndpoint(
-      int activitySourceId,
-      string jobId,
-      DistributionQueryTaskResult distributionQueryTaskResult)
+        // a null job is impossible because the necessary JSON payload
+        // to achieve it would fail deserialization
+        _logger.LogInformation("Found Query with Id: {JobId}", job!.JobId);
+        //Set ActivitySource ID
+        job.ActivitySourceId = activitySource.Id;
+        return job;
+      }
+      catch (JsonException e)
+      {
+        _logger.LogError(e, "Invalid Response Format from Fetch Query Endpoint");
+
+        var body = await result.Content.ReadAsStringAsync();
+        _logger.LogDebug("Invalid Response Body: {Body}", body);
+
+        throw;
+      }
+    }
+    else
     {
-      // Get activitySource
-      var activitySource = await _db.ActivitySources
-        .FirstOrDefaultAsync(x => x.Id == activitySourceId);
-
-      if (activitySource is null)
-        throw new KeyNotFoundException(
-          $"No ActivitySource with ID: {activitySourceId}");
-
-      // Make URL to send the file
-      var fileUrl = Url.Combine(
-        activitySource.Host,
-        _apiOptions.EndpointBase,
-        _apiOptions.ResultFileEndpoint,
-        jobId,
-        activitySource.ResourceId);
-
-      // Send the file
-      var fileUploadResponse = (await _client.PostAsync(
-          fileUrl, AsHttpJsonString(distributionQueryTaskResult))
-        ).EnsureSuccessStatusCode();
-
-      // Check it worked
-      var fileUploadBody = fileUploadResponse.Content.ReadAsStringAsync();
-      // Todo: Check the response is okay.
-
-      // Make URL for step 2 in results submission
-      var secondStepUrl = Url.Combine(
-        activitySource.Host,
-        _apiOptions.EndpointBase,
-        _apiOptions.SubmitResultEndpoint,
-        jobId,
-        activitySource.ResourceId);
-      
-      // Remove the files before uploading the results to the second endpoint
-      distributionQueryTaskResult.QueryResult.Files.Clear();
-      
-      var secondStepResponse = (await _client.PostAsync(
-          secondStepUrl, AsHttpJsonString(distributionQueryTaskResult))
-        ).EnsureSuccessStatusCode();
-      
-      // Check it worked
-      var secondStepBody = secondStepResponse.Content.ReadAsStringAsync();
-      // Todo: Check the response is okay.
+      _logger.LogError("Fetch Query Endpoint Request failed: {StatusCode}", result.StatusCode);
+      throw new ApplicationException($"Fetch Query Endpoint Request failed: {result.StatusCode}");
     }
+  }
+
+  /// <summary>
+  /// Post to the Results endpoint, and handle the response correctly
+  /// </summary>
+  /// <param name="activitySourceId">activitySourceId ID</param>
+  /// <param name="jobId">Job ID</param>
+  /// <param name="result">Results with Count</param>
+  public async Task ResultsEndpointPost(int activitySourceId, string jobId, QueryResultCount result)
+  {
+    var activitySource = await _db.ActivitySources
+      .FirstOrDefaultAsync(x => x.Id == activitySourceId);
+
+    if (activitySource is null)
+      throw new KeyNotFoundException(
+        $"No ActivitySource with ID: {activitySourceId}");
+
+    var requestUri = Url.Combine(
+      activitySource.Host,
+      _apiOptions.EndpointBase,
+      _apiOptions.SubmitResultEndpoint,
+      jobId,
+      activitySource.ResourceId);
+
+    var response = (await _client.PostAsync(
+        requestUri, AsHttpJsonString(new RquestQueryTaskResult(activitySource.ResourceId, jobId, result.Count))))
+      .EnsureSuccessStatusCode();
+
+    var body = await response.Content.ReadAsStringAsync();
+
+    if (body != "Job saved")
+    {
+      const string message = "Unsuccessful Response from Submit Results Endpoint";
+      _logger.LogError(message);
+      _logger.LogDebug("Response Body: {Body}", body);
+
+      throw new ApplicationException(message);
+    }
+
+    return;
+  }
+
+  public async Task DistributionResultsEndpoint(
+    int activitySourceId,
+    string jobId,
+    DistributionQueryTaskResult distributionQueryTaskResult)
+  {
+    // Get activitySource
+    var activitySource = await _db.ActivitySources
+      .FirstOrDefaultAsync(x => x.Id == activitySourceId);
+
+    if (activitySource is null)
+      throw new KeyNotFoundException(
+        $"No ActivitySource with ID: {activitySourceId}");
+
+    // Make URL to send the file
+    var fileUrl = Url.Combine(
+      activitySource.Host,
+      _apiOptions.EndpointBase,
+      _apiOptions.ResultFileEndpoint,
+      jobId,
+      activitySource.ResourceId);
+
+    // Send the file
+    var fileUploadResponse = (await _client.PostAsync(
+        fileUrl, AsHttpJsonString(distributionQueryTaskResult))
+      ).EnsureSuccessStatusCode();
+
+    // Check it worked
+    var fileUploadBody = fileUploadResponse.Content.ReadAsStringAsync();
+    // Todo: Check the response is okay.
+
+    // Make URL for step 2 in results submission
+    var secondStepUrl = Url.Combine(
+      activitySource.Host,
+      _apiOptions.EndpointBase,
+      _apiOptions.SubmitResultEndpoint,
+      jobId,
+      activitySource.ResourceId);
+
+    // Remove the files before uploading the results to the second endpoint
+    distributionQueryTaskResult.QueryResult.Files.Clear();
+
+    var secondStepResponse = (await _client.PostAsync(
+        secondStepUrl, AsHttpJsonString(distributionQueryTaskResult))
+      ).EnsureSuccessStatusCode();
   }
 }
