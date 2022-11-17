c492398dfa62b8e73d37e53395f280f1bd837905
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-11-17 11:29:32 +0000

    handle nulls

--- app/HutchManager/Services/ResultsTranslator.cs
+++ app/HutchManager/Services/ResultsTranslator.cs
@@ -21,18 +21,19 @@ public class ResultsTranslator
       foreach (var graph in job.Graphs)
       {
         var property = SchemaSerializer.DeserializeObject<PropertyValue>(graph.ToString());
+        if (property == null) throw new InvalidDataException();
         switch (property.Name)
         {
           case "activity_source_id":
-            int activitySourceId = int.Parse(property.Value);
+            int activitySourceId = int.Parse(property.Value!);
             rquestQueryResult.ActivitySourceId = activitySourceId;
             break;
           case "job_id":
             var jobId = property.Value;
-            rquestQueryResult.JobId = jobId;
+            rquestQueryResult.JobId = jobId!; 
             break;
           case "count":
-            int count = int.Parse(property.Value);
+            int count = int.Parse(property.Value!);
             rquestQueryResult.Results.Count = count;
             break;
         }
@@ -54,62 +55,63 @@ public class ResultsTranslator
           {
             case "PropertyValue":
               var property = SchemaSerializer.DeserializeObject<PropertyValue>(graph.ToString());
+              if (property == null) throw new InvalidDataException();
               switch (property.Name)
               {
                 case "activity_source_id":
-                  int activitySourceId = int.Parse(property.Value);
+                  int activitySourceId = int.Parse(property.Value!);
                   rquestQueryResult.ActivitySourceId = activitySourceId;
                   break;
                 case "status":
-                  rquestQueryResult.Status = property.Value;
+                  rquestQueryResult.Status = property.Value!;
                   break;
                 case "job_id":
-                  rquestQueryResult.JobId = property.Value;
+                  rquestQueryResult.JobId = property.Value!;
                   break;
                 case "count":
-                  rquestQueryResult.QueryResult.Count = int.Parse(property.Value);
+                  rquestQueryResult.QueryResult.Count = int.Parse(property.Value!);
                   break;
                 case "datasetCount":
-                  rquestQueryResult.QueryResult.DatasetsCount = int.Parse(property.Value);
+                  rquestQueryResult.QueryResult.DatasetsCount = int.Parse(property.Value!);
                   break;
               }
               break;
             case "ItemList":
               var itemList = SchemaSerializer.DeserializeObject<ItemList>(graph.ToString());
+              if (itemList == null) throw new InvalidDataException();
               var fileObject = new FileObject();
               foreach (var item in itemList.ItemListElement)
               {
                 var fileProperty = SchemaSerializer.DeserializeObject<PropertyValue>(item.ToString());
+                if (fileProperty == null) throw new InvalidDataException();
                 switch (fileProperty.Name)
                 {
                   case "fileData":
-                    fileObject.Data = fileProperty.Value.ToString();
+                    fileObject.Data = fileProperty.Value.ToString()!;
                     break;
                   case "fileDescription":
                     fileObject.Description = fileProperty.Value;
                     break;
                   case "fileName":
-                    fileObject.Name = fileProperty.Value;
+                    fileObject.Name = fileProperty.Value!;
                     break;
                   case "fileReference":
-                    fileObject.Reference = fileProperty.Value;
+                    fileObject.Reference = fileProperty.Value!;
                     break;
                   case "fileSensitive":
-                    fileObject.Sensitive = (bool)fileProperty.Value;
+                    fileObject.Sensitive = (bool)fileProperty.Value!;
                     break;
                   case "fileSize":
-                    fileObject.Size = (double)fileProperty.Value;
+                    fileObject.Size = (double)fileProperty.Value!;
                     break;
                   case "fileType":
-                    fileObject.Type = fileProperty.Value;
+                    fileObject.Type = fileProperty.Value!;
                     break;
                 }
               }
               break;
           }
         }
-        
-
       }
       return rquestQueryResult;
     }
