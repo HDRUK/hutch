e85b19a74f242a4e4ca394c70333b5880b57a920
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-12-06 15:40:26 +0000

    remove db logging setup from main.py

--- app/HutchAgent/scripts/main.py
+++ app/HutchAgent/scripts/main.py
@@ -5,8 +5,6 @@ import hutchagent.config as config
 import pika
 import dotenv
 import hutchagent.message_queues.rmq_queue as rmq
-from hutchagent.db_manager import SyncDBManager
-from hutchagent.db_logging import SyncLogDBHandler
 from hutchagent.checkin import CheckIn
 
 
@@ -19,34 +17,12 @@ def main():
         datefmt=config.DATE_FORMAT,
     )
 
-    # set up the backup logger
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(LOG_FORMAT)
-    backup_logger = logging.getLogger(config.BACKUP_LOGGER_NAME)
-    backup_logger.setLevel(logging.INFO)
-    backup_logger.addHandler(console_handler)
-
-    # set up the db logger
-    log_db_host = os.getenv("LOG_DB_HOST")
-    log_db_port = os.getenv("LOG_DB_PORT")
-
+    # set up the logger
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(LOG_FORMAT)
     logger = logging.getLogger(config.LOGGER_NAME)
     logger.setLevel(logging.INFO)
-    if log_db_host is not None:
-        db_manager = SyncDBManager(
-            username=os.getenv("LOG_DB_USERNAME"),
-            password=os.getenv("LOG_DB_PASSWORD"),
-            host=log_db_host,
-            port=int(log_db_port) if log_db_port is not None else None,
-            database=os.getenv("LOG_DB_DATABASE"),
-            drivername=os.getenv("LOG_DB_DRIVERNAME", config.DEFAULT_DB_DRIVER),
-        )
-        db_handler = SyncLogDBHandler(db_manager, config.BACKUP_LOGGER_NAME)
-        db_handler.setFormatter(LOG_FORMAT)
-
-        logger.addHandler(db_handler)
-
-    logger.addHandler(console_handler)
+    logger.addHandler(handler)
 
     # set up check-in thread
     check_in_thread = CheckIn(
@@ -72,7 +48,7 @@ def main():
         logger.info("Successfully connected to queue. Press Ctrl+C to exit.")
         channel.start_consuming()  # starts a `while True` loop.
     except pika.exceptions.AMQPConnectionError:
-        logger.critical("Unable to connect to queue. Exiting...", exc_info=True)
+        logger.critical("Unable to connect to queue. Exiting...")
     except KeyboardInterrupt:
         # shut down on Ctrl+C
         logger.info("Disconnecting from queue...")
