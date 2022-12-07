cae87264b328e07b3e5401bd35c3a588e6ed5151
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2022-12-07 11:22:36 +0000

    de-ro_crates file.py

--- app/HutchAgent/hutchagent/rquest/file.py
+++ app/HutchAgent/hutchagent/rquest/file.py
@@ -1,5 +1,3 @@
-from hutchagent.ro_crates.property_value import PropertyValue
-
 class File:
     def __init__(
         self,
@@ -20,50 +18,4 @@ class File:
         self.type_ = type_
 
     def to_dict(self) -> dict:
-        return {
-            "@context": "https://w3id.org/ro/crate/1.1/context",
-            "@graph": [
-                PropertyValue(
-                    context="https://schema.org",
-                    type_="PropertyValue",
-                    name="fileData",
-                    value=self.data,
-                ).to_dict(),
-                PropertyValue(
-                    context="https://schema.org",
-                    type_="PropertyValue",
-                    name="fileDescription",
-                    value=self.description,
-                ).to_dict(),
-                PropertyValue(
-                    context="https://schema.org",
-                    type_="PropertyValue",
-                    name="fileName",
-                    value=self.name,
-                ).to_dict(),
-                PropertyValue(
-                    context="https://schema.org",
-                    type_="PropertyValue",
-                    name="fileReference",
-                    value=self.reference,
-                ).to_dict(),
-                PropertyValue(
-                    context="https://schema.org",
-                    type_="PropertyValue",
-                    name="fileSensitive",
-                    value=self.sensitive,
-                ).to_dict(),
-                PropertyValue(
-                    context="https://schema.org",
-                    type_="PropertyValue",
-                    name="fileSize",
-                    value=self.size,
-                ).to_dict(),
-                PropertyValue(
-                    context="https://schema.org",
-                    type_="PropertyValue",
-                    name="fileType",
-                    value=self.type_,
-                ).to_dict(),
-            ],
-        }
+        return {}
