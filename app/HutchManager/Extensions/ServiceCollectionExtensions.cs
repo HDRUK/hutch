ed3588ff6d118f61fb649c04c74ca32caef34e0c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-01-17 09:59:07 +0000

    AB#89730: Migrations can't be generated due to no default queue config (#147)

--- app/HutchManager/Extensions/ServiceCollectionExtensions.cs
+++ app/HutchManager/Extensions/ServiceCollectionExtensions.cs
@@ -10,49 +10,140 @@ namespace HutchManager.Extensions
 {
   public static class ServiceCollectionExtensions
   {
-    public static IServiceCollection AddEmailSender(this IServiceCollection s, IConfiguration c)
+    /// <summary>
+    /// Enum for Email Providers.
+    /// </summary>
+    enum EmailProviders
     {
-      var emailProvider = c["OutboundEmail:Provider"] ?? string.Empty;
+      Local,
+      SendGrid,
+      SMTP
+    }
+    
+    /// <summary>
+    /// Enum for Job Queue Providers.
+    /// </summary>
+    enum JobQueueProviders
+    {
+      RabbitMq,
+      AzureQueueStorage
+    }
+    
+    /// <summary>
+    /// Parse the desired Email Provider from configuration.
+    /// </summary>
+    /// <param name="c">The configuration object</param>
+    /// <returns>The <c>enum</c> for the chosen Email Provider</returns>
+    private static EmailProviders GetEmailProvider(IConfiguration c)
+    {
+      // TryParse defaults failures to the first enum value.
+      Enum.TryParse<EmailProviders>(
+        c["OutboundEmail:Provider"],
+        ignoreCase: true,
+        out var emailProvider);
 
-      var useSendGrid = emailProvider.Equals("sendgrid", StringComparison.InvariantCultureIgnoreCase);
-      var useSmtp = emailProvider.Equals("smtp", StringComparison.InvariantCultureIgnoreCase);
+      return emailProvider;
+    }
+    
+    private static IServiceCollection ConfigureEmail(
+      this IServiceCollection s,
+      EmailProviders provider,
+      IConfiguration c)
+    {
+      // set the appropriate configuration function
+      Func<IConfiguration, IServiceCollection> ConfigureEmail =
+        provider switch
+        {
+          EmailProviders.SendGrid => s.Configure<SendGridOptions>,
+          //EmailProviders.SMTP => s.Configure<
+          _ => s.Configure<LocalDiskEmailOptions>
+        };
 
-      if (useSendGrid) s.Configure<SendGridOptions>(c.GetSection("OutboundEmail"));
-      else if (useSmtp) s.Configure<SmtpOptions>(c.GetSection("OutboundEmail"));
-      else s.Configure<LocalDiskEmailOptions>(c.GetSection("OutboundEmail"));
-
-      s
-        .AddTransient<TokenIssuingService>()
-        .AddTransient<RazorViewService>()
-        .AddTransient<AccountEmailService>()
-        .TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
-
-      if (useSendGrid) s.AddTransient<IEmailSender, SendGridEmailSender>();
-      else if (useSmtp) s.AddTransient<IEmailSender, SmtpEmailSender>();
-      else s.AddTransient<IEmailSender, LocalDiskEmailSender>();
+      // and execute it
+      ConfigureEmail.Invoke(c.GetSection("OutboundEmail"));
 
       return s;
     }
-
-    public static IServiceCollection AddJobQueue(this IServiceCollection s, IConfiguration c)
+    
+    /// <summary>
+    /// Determine which email sender the user wishes to use and adds it the service collection.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddEmailSender(this IServiceCollection s, IConfiguration c)
     {
-      var queueType = c["QueueType"] ?? throw new Exception("Please provide a queue type");
+      var emailProvider = GetEmailProvider(c);
 
-      switch (queueType)
-      {
-        case "AzureQueueStorage":
-          s.Configure<AzureJobQueueOptions>(c.GetSection("JobQueue"))
-            .AddTransient<IJobQueueService, AzureJobQueueService>();
-          break;
-        case "RabbitMQ":
-          s.Configure<RabbitJobQueueOptions>(c.GetSection("JobQueue"))
-            .AddTransient<IJobQueueService, RabbitJobQueueService>();
-          break;
-        default:
-          throw new Exception($"'{queueType}' is not a valid queue service");
-      }
+      s.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
+
+      return s.ConfigureEmail(emailProvider, c)
+        .AddTransient<TokenIssuingService>()
+        .AddTransient<RazorViewService>()
+        .AddTransient<AccountEmailService>()
+        .AddTransient(
+          typeof(IEmailSender),
+          emailProvider switch
+          {
+            EmailProviders.SendGrid => typeof(SendGridEmailSender),
+            
+            _ => typeof(LocalDiskEmailSender)
+          });
+    }
+
+    /// <summary>
+    /// Parse the desired Job Queue Provider from configuration.
+    /// </summary>
+    /// <param name="c">The configuration object</param>
+    /// <returns>The <c>enum</c> for the chosen Job Queue Provider</returns>
+    private static JobQueueProviders GetJobQueueProvider(IConfiguration c)
+    {
+      Enum.TryParse<JobQueueProviders>(
+        c["JobQueue:Provider"],
+        ignoreCase: true,
+        out var jobQueueProvider);
+
+      return jobQueueProvider;
+    }
+    
+    private static IServiceCollection ConfigureJobQueue(
+      this IServiceCollection s,
+      JobQueueProviders provider,
+      IConfiguration c)
+    {
+      // set the appropriate configuration function
+      Func<IConfiguration, IServiceCollection> ConfigureEmail =
+        provider switch
+        {
+          JobQueueProviders.RabbitMq => s.Configure<AzureJobQueueOptions>,
+          JobQueueProviders.AzureQueueStorage => s.Configure<AzureJobQueueOptions>,
+          _ => throw new ArgumentOutOfRangeException(nameof(provider), provider, null)
+        };
+
+      // and execute it
+      ConfigureEmail.Invoke(c.GetSection("JobQueue"));
 
       return s;
+    }
+    
+    /// <summary>
+    /// Determine which job queue the user wishes to use and adds it the service collection.
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddJobQueue(this IServiceCollection s, IConfiguration c)
+    {
+      var queueType = GetJobQueueProvider(c);
+
+      return s.ConfigureJobQueue(queueType, c)
+        .AddTransient(
+          typeof(IJobQueueService),
+          queueType switch
+          {
+            JobQueueProviders.AzureQueueStorage => typeof(AzureJobQueueService),
+            _ => typeof(RabbitJobQueueService)
+          });
     }
   }
 }
