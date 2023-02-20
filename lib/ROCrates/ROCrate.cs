82a9e608239747921bb59b53ea2fce82a412597b
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-02-20 15:37:51 +0000

    ROCrate constructor

--- lib/ROCrates/ROCrate.cs
+++ lib/ROCrates/ROCrate.cs
@@ -1,5 +1,27 @@
-﻿namespace ROCrates;
+﻿using ROCrates.Models;
+
+namespace ROCrates;
 public class ROCrate
 {
+  private List<ContextEntity> _contextEntities = new();
+  private List<DataEntity> _dataEntities = new();
+  private List<Entity> _defaultEntities = new();
+  private Dictionary<string, Entity> _entityMap = new();
+  private string _uuid = Guid.NewGuid().ToString();
+  /// TODO: add the following fields:
+  ///   - preview (based on Preview class not yet made)
+  ///   - arcp_base_uri ($"arcp://uuid,{_uuid}")
 
+  private string _source;
+  private List<string>? _exclude;
+  private bool _generatePreview;
+  private bool _init;
+
+  public ROCrate(string source, bool generatePreview = false, bool init = false, List<string>? exclude = null)
+  {
+    _source = source;
+    _generatePreview = generatePreview;
+    _init = init;
+    _exclude = exclude;
+  }
 }
