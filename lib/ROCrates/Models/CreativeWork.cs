c5c94ed5b2b2b5392fefba3af3672f3cf4bb65af
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-02-20 14:40:50 +0000

    Add constructors

--- lib/ROCrates/Models/CreativeWork.cs
+++ lib/ROCrates/Models/CreativeWork.cs
@@ -1,6 +1,10 @@
+using System.Text.Json.Nodes;
+
 namespace ROCrates.Models;
 
 public class CreativeWork : Entity
 {
-  
+  public CreativeWork(ROCrate crate, string? identifier, JsonObject? properties) : base(crate, identifier, properties)
+  {
+  }
 }
