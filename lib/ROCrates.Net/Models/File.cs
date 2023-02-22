e1128bfaee8ff057a6b80a606c43e8f969e2bf5f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-02-22 15:46:30 +0000

    docstring on File.cs

--- lib/ROCrates.Net/Models/File.cs
+++ lib/ROCrates.Net/Models/File.cs
@@ -4,6 +4,9 @@ using System.Text.Json.Nodes;
 
 namespace ROCrates.Models;
 
+/// <summary>
+/// This class represents a generic file entity that can go inside an RO-Crate.
+/// </summary>
 public class File : FileOrDir
 {
   private const string _defaultType = "File";
@@ -24,6 +27,33 @@ public class File : FileOrDir
     }
   }
 
+  /// <summary>
+  ///   <para>Write file contents to the specified path. e.g. The root path of an RO-Crate.</para>
+  ///   <para>
+  ///     If the file is a remote file, and <c>fetchUrl</c> is set to <c>true</c>, the file will be downloaded under
+  ///     "<c>basePath</c>".
+  ///   </para>
+  ///   <para>
+  ///     If the file is on disk, it will be copied to a new location under "<c>basePath</c>".
+  ///   </para>
+  ///   <para>
+  ///     In either case, the file will be saved to "<c>basePath/Identifier</c>"
+  ///   </para>
+  /// </summary>
+  /// <example>
+  /// <code>
+  /// var url = "https://hdruk.github.io/hutch/docs/devs";
+  /// var fileName = url.Split('/').Last();
+  /// var fileEntity = new Models.File(
+  ///    new ROCrate("myCrate.zip"),
+  ///    source: url,
+  ///    validateUrl: true,
+  ///    fetchRemote: true);
+  /// fileEntity.Write("myCrate");
+  /// Assert.True(File.Exists(Path.Combine("myCrate", fileName)));
+  /// </code>
+  /// </example>
+  /// <param name="basePath">The path the file will be written to.</param>
   public void Write(string basePath)
   {
     var outFilePath = Path.Join(basePath, Identifier);
