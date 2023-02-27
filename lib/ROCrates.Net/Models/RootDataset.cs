c1ba84b552c705f09a26476a2b720508afc14ae0
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-02-27 16:56:44 +0000

    create RootDataset.cs

---
+++ lib/ROCrates.Net/Models/RootDataset.cs
@@ -0,0 +1,14 @@
+using System.Text.Json.Nodes;
+
+namespace ROCrates.Models;
+
+public class RootDataset : Dataset
+{
+  public RootDataset(ROCrate crate, string? identifier = null, JsonObject? properties = null, string source = "./",
+    string? destPath = null, bool fetchRemote = false, bool validateUrl = false) : base(crate, identifier, properties,
+    source, destPath, fetchRemote, validateUrl)
+  {
+    Properties = _empty();
+    if (properties is not null) _unpackPropterties(properties);
+  }
+}
