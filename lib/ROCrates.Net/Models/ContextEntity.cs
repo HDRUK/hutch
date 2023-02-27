d721efeee129ae23e3531af007d1ab533ccb9f86
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-02-27 15:19:53 +0000

    finish ContextEntity.cs

--- lib/ROCrates.Net/Models/ContextEntity.cs
+++ lib/ROCrates.Net/Models/ContextEntity.cs
@@ -7,5 +7,14 @@ public class ContextEntity : Entity
   public ContextEntity(ROCrate crate, string? identifier = null, JsonObject? properties = null) : base(crate,
     identifier, properties)
   {
+    Identifier = _formatIdentifier(Identifier);
+  }
+
+  protected sealed override string _formatIdentifier(string identifier)
+  {
+    if (Uri.IsWellFormedUriString(identifier, UriKind.RelativeOrAbsolute) || identifier.Contains('#'))
+      return identifier;
+    
+    return "#" + identifier;
   }
 }
