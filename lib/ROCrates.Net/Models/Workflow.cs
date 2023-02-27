383c4207d6da18ae2cfbd911a585223d87669644
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-02-27 17:52:44 +0000

    created workflow classes

---
+++ lib/ROCrates.Net/Models/Workflow.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Nodes;
+
+namespace ROCrates.Models;
+
+/// <summary>
+/// Legacy workflow, added for completeness.
+/// </summary>
+public class Workflow : ComputationalWorkflow
+{
+  public Workflow(ROCrate crate, string? identifier = null, JsonObject? properties = null, string source = "./",
+    string? destPath = null, bool fetchRemote = false, bool validateUrl = false) : base(crate, identifier, properties,
+    source, destPath, fetchRemote, validateUrl)
+  {
+    Types = new []{ "File", "SoftwareSourceCode", "Workflow" };
+    SetProperty("@type", Types);
+  }
+}
---
+++ lib/ROCrates.Net/Models/WorkflowDescription.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Nodes;
+
+namespace ROCrates.Models;
+
+/// <summary>
+/// Abstract CWL description of the main workflow.
+/// </summary>
+public class WorkflowDescription : ComputationalWorkflow
+{
+  public WorkflowDescription(ROCrate crate, string? identifier = null, JsonObject? properties = null,
+    string source = "./", string? destPath = null, bool fetchRemote = false, bool validateUrl = false) : base(crate,
+    identifier, properties, source, destPath, fetchRemote, validateUrl)
+  {
+    Types = new[] { "File", "SoftwareSourceCode", "HowTo" };
+    SetProperty("@type", Types);
+  }
+}
