0f5231cb83cb1dc8d938f86f7c4dadb33567a4f8
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-02-27 13:37:00 +0000

    _getParts methods

--- lib/ROCrates.Net/Models/Dataset.cs
+++ lib/ROCrates.Net/Models/Dataset.cs
@@ -16,6 +16,7 @@ public class Dataset : FileOrDir
   public override void Write(string basePath)
   {
     var outFilePath = Path.Join(basePath, Identifier);
+    var outFileParent = Path.GetDirectoryName(outFilePath);
     if (Uri.IsWellFormedUriString(_source, UriKind.Absolute))
     {
       if (_validateUrl && !_fetchRemote)
@@ -27,13 +28,32 @@ public class Dataset : FileOrDir
 
       if (_fetchRemote)
       {
-        
+        _getParts(basePath);
       }
+    }
+    else
+    {
+      // Copy files on system
     }
   }
 
   private void _getParts(string outPath)
   {
-    
+    Directory.CreateDirectory(outPath);
+    var basePath = _source.TrimEnd('/');
+    var parts = GetProperty<Dictionary<string, string>>("hasPart");
+    if (parts is null) return;
+    using var httpClient = new HttpClient();
+    foreach (var p in parts)
+    {
+      var part = p.Key == "@id" ? p.Value : null;
+      if (part is null) continue;
+      var partUri = $"{basePath}/{part}";
+      var partOutPath = Path.Combine(outPath, part);
+      var response = httpClient.GetAsync(partUri).Result.Content;
+      using var httpStream = response.ReadAsStream();
+      using var file = System.IO.File.OpenWrite(partOutPath);
+      httpStream.CopyTo(file);
+    }
   }
 }
