9f3bbb308661bf4f0d39357c2d59d8e5149ca9c6
Daniel Lea <daniel.lea@nottingham.ac.uk>
2023-03-10 15:34:22 +0000

    aarange-assert-act in TestFile.cs

--- lib/ROCrates.Net.Tests/TestFile.cs
+++ lib/ROCrates.Net.Tests/TestFile.cs
@@ -14,28 +14,39 @@ public class TestFile : IClassFixture<TestFileFixture>
   [Fact]
   public void TestWrite_Saves_To_DestPath()
   {
+    // Arrange
     var testDestPath = Path.Combine(_testBasePath, "ext", _testFileFixture.TestFileName);
     var fileEntity = new Models.File(
       new ROCrate(_testFileFixture.TestFileName),
       source: Path.Combine(_testBasePath, _testFileFixture.TestFileName),
       destPath: testDestPath);
+
+    // Act
     fileEntity.Write(_testBasePath);
+
+    // Assert
     Assert.True(File.Exists(Path.Combine(_testBasePath, testDestPath)));
   }
-  
+
   [Fact]
   public void TestWrite_Saves_To_Source()
   {
+    // Arrange
     var fileEntity = new Models.File(
       new ROCrate(_testFileFixture.TestFileName),
       source: Path.Combine(_testBasePath, _testFileFixture.TestFileName));
+
+    // Act
     fileEntity.Write(_testBasePath);
+
+    // Assert
     Assert.True(File.Exists(Path.Combine(_testBasePath, _testFileFixture.TestFileName)));
   }
-  
+
   [Fact]
   public void TestWrite_Saves_From_Remote()
   {
+    // Arrange
     var url = "https://hdruk.github.io/hutch/docs/devs";
     var fileName = url.Split('/').Last();
     var fileEntity = new Models.File(
@@ -43,7 +54,11 @@ public class TestFile : IClassFixture<TestFileFixture>
       source: url,
       validateUrl: true,
       fetchRemote: true);
+
+    // Act
     fileEntity.Write(_testBasePath);
+
+    // Assert
     Assert.True(File.Exists(Path.Combine(_testBasePath, fileName)));
   }
 }
@@ -52,12 +67,14 @@ public class TestFileFixture : IDisposable
 {
   public readonly string TestFileName = "my-test-file.txt";
   public readonly string TestBasePath = Path.Combine("file", "test", "path");
+
   public TestFileFixture()
   {
     Directory.CreateDirectory(TestBasePath);
     using var file = new StreamWriter(Path.Combine(TestBasePath, TestFileName));
     file.WriteLine("some content");
   }
+
   public void Dispose()
   {
     File.Delete(TestFileName);
