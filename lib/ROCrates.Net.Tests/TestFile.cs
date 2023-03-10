23c7e775c55c8d27a6aa497734954fe98fca87ab
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-03-10 15:06:34 +0000

    use fixture file name

--- lib/ROCrates.Net.Tests/TestFile.cs
+++ lib/ROCrates.Net.Tests/TestFile.cs
@@ -1,17 +1,12 @@
-using Xunit.Abstractions;
-
 namespace ROCrates.Tests;
 
 public class TestFile : IClassFixture<TestFileFixture>
 {
-  private readonly ITestOutputHelper _testOutputHelper;
-  private TestFileFixture _testFileFixture;
-  private const string _testFileName = "my-test-file.txt";
+  private readonly TestFileFixture _testFileFixture;
   private readonly string _testBasePath;
 
-  public TestFile(ITestOutputHelper testOutputHelper, TestFileFixture testFileFixture)
+  public TestFile(TestFileFixture testFileFixture)
   {
-    _testOutputHelper = testOutputHelper;
     _testFileFixture = testFileFixture;
     _testBasePath = testFileFixture.TestBasePath;
   }
@@ -19,10 +14,10 @@ public class TestFile : IClassFixture<TestFileFixture>
   [Fact]
   public void TestWrite_Saves_To_DestPath()
   {
-    var testDestPath = Path.Combine(_testBasePath, "ext", _testFileName);
+    var testDestPath = Path.Combine(_testBasePath, "ext", _testFileFixture.TestFileName);
     var fileEntity = new Models.File(
-      new ROCrate(_testFileName),
-      source: Path.Combine(_testBasePath, _testFileName),
+      new ROCrate(_testFileFixture.TestFileName),
+      source: Path.Combine(_testBasePath, _testFileFixture.TestFileName),
       destPath: testDestPath);
     fileEntity.Write(_testBasePath);
     Assert.True(File.Exists(Path.Combine(_testBasePath, testDestPath)));
@@ -32,10 +27,10 @@ public class TestFile : IClassFixture<TestFileFixture>
   public void TestWrite_Saves_To_Source()
   {
     var fileEntity = new Models.File(
-      new ROCrate(_testFileName),
-      source: Path.Combine(_testBasePath, _testFileName));
+      new ROCrate(_testFileFixture.TestFileName),
+      source: Path.Combine(_testBasePath, _testFileFixture.TestFileName));
     fileEntity.Write(_testBasePath);
-    Assert.True(File.Exists(Path.Combine(_testBasePath, _testFileName)));
+    Assert.True(File.Exists(Path.Combine(_testBasePath, _testFileFixture.TestFileName)));
   }
   
   [Fact]
@@ -44,7 +39,7 @@ public class TestFile : IClassFixture<TestFileFixture>
     var url = "https://hdruk.github.io/hutch/docs/devs";
     var fileName = url.Split('/').Last();
     var fileEntity = new Models.File(
-      new ROCrate(_testFileName),
+      new ROCrate(_testFileFixture.TestFileName),
       source: url,
       validateUrl: true,
       fetchRemote: true);
@@ -55,8 +50,7 @@ public class TestFile : IClassFixture<TestFileFixture>
 
 public class TestFileFixture : IDisposable
 {
-  private static char _sep = Path.DirectorySeparatorChar;
-  public const string TestFileName = "my-test-file.txt";
+  public readonly string TestFileName = "my-test-file.txt";
   public readonly string TestBasePath = Path.Combine("file", "test", "path");
   public TestFileFixture()
   {
