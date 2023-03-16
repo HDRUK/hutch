fade34f42fc72bbcd1f87ed32cdc5d7c978ed2f6
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-03-16 13:30:44 +0000

    TestSuiteConverter.cs

---
+++ lib/ROCrates.Net/Converters/TestSuiteConverter.cs
@@ -0,0 +1,6 @@
+namespace ROCrates.Converters;
+
+public class TestSuiteConverter : EntityConverter
+{
+  // Todo: override `Read` to return a TestSuite
+}
--- lib/ROCrates.Net/Models/TestSuite.cs
+++ lib/ROCrates.Net/Models/TestSuite.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using ROCrates.Converters;
 
 namespace ROCrates.Models;
 
@@ -11,22 +13,37 @@ public class TestSuite : ContextEntity
     Properties = _empty();
     if (properties is not null) _unpackProperties(properties);
   }
-  
+
   public string? EngineVersion
   {
     get => GetProperty<string>("engineVersion");
     set => SetProperty("engineVersion", value);
   }
-  
+
   public Part? ConformsTo
   {
     get => GetProperty<Part>("conformsTo");
     set => SetProperty("conformsTo", value);
   }
-  
+
   public string? Name
   {
     get => GetProperty<string>("name");
     set => SetProperty("name", value);
+  }
+
+  /// <summary>
+  /// Convert <see cref="TestSuite"/> to JSON string.
+  /// </summary>
+  /// <returns>The <see cref="TestSuite"/> as a JSON string.</returns>
+  public override string Serialize()
+  {
+    var options = new JsonSerializerOptions
+    {
+      WriteIndented = true,
+      Converters = { new TestSuiteConverter() }
+    };
+    var serialised = JsonSerializer.Serialize(this, options);
+    return serialised;
   }
 }
