2ff2b961a8d3e29f55277307019dbc2034428127
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-03-16 11:32:11 +0000

    FileConverter.cs

---
+++ lib/ROCrates.Net/Converters/FileConverter.cs
@@ -0,0 +1,6 @@
+namespace ROCrates.Converters;
+
+public class FileConverter : EntityConverter
+{
+  // Todo: override `Read` to return a File
+}
--- lib/ROCrates.Net/Models/File.cs
+++ lib/ROCrates.Net/Models/File.cs
@@ -1,5 +1,7 @@
 using System.Globalization;
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using ROCrates.Converters;
 
 namespace ROCrates.Models;
 
@@ -73,5 +75,20 @@ public class File : FileOrDir
       Directory.CreateDirectory(outFileParent);
       System.IO.File.Copy(_source, outFilePath, overwrite: true);
     }
+  }
+
+  /// <summary>
+  /// Convert <see cref="File"/> to JSON string.
+  /// </summary>
+  /// <returns>The <see cref="File"/> as a JSON string.</returns>
+  public override string Serialize()
+  {
+    var options = new JsonSerializerOptions
+    {
+      WriteIndented = true,
+      Converters = { new FileConverter() }
+    };
+    var serialised = JsonSerializer.Serialize(this, options);
+    return serialised;
   }
 }
