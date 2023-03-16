84286e1bc5333a77f54b39c607943c2ac15e0f5c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-03-16 13:33:34 +0000

    WorkflowConverter.cs

---
+++ lib/ROCrates.Net/Converters/WorkflowConverter.cs
@@ -0,0 +1,6 @@
+namespace ROCrates.Converters;
+
+public class WorkflowConverter : EntityConverter
+{
+  // Todo: override `Read` to return a Workflow
+}
--- lib/ROCrates.Net/Models/Workflow.cs
+++ lib/ROCrates.Net/Models/Workflow.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using System.Text.Json.Nodes;
+using ROCrates.Converters;
 
 namespace ROCrates.Models;
 
@@ -11,7 +13,22 @@ public class Workflow : ComputationalWorkflow
     string? destPath = null, bool fetchRemote = false, bool validateUrl = false) : base(crate, identifier, properties,
     source, destPath, fetchRemote, validateUrl)
   {
-    Types = new []{ "File", "SoftwareSourceCode", "Workflow" };
+    Types = new[] { "File", "SoftwareSourceCode", "Workflow" };
     SetProperty("@type", Types);
+  }
+
+  /// <summary>
+  /// Convert <see cref="Workflow"/> to JSON string.
+  /// </summary>
+  /// <returns>The <see cref="Workflow"/> as a JSON string.</returns>
+  public override string Serialize()
+  {
+    var options = new JsonSerializerOptions
+    {
+      WriteIndented = true,
+      Converters = { new WorkflowConverter() }
+    };
+    var serialised = JsonSerializer.Serialize(this, options);
+    return serialised;
   }
 }
