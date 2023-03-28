a871430ce310fca59dc629f0a1f08e785c2ae811
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-03-28 15:54:25 +0100

    Add path to config files to WorkflowTriggerOptions.cs

--- app/HutchAgent/Models/WorkflowTriggerOptions.cs
+++ app/HutchAgent/Models/WorkflowTriggerOptions.cs
@@ -4,8 +4,12 @@ public class WorkflowTriggerOptions
 {
   public const string Type = "WfExS";
 
-  public const string ExecutorPath = "/Users/vasilikipanagi/Projects/WfExS-backend/";
+  public const string ExecutorPath = "/home/vasiliki/WfExS-backend/";
 
-  public const string VirtualEnvironmentPath = "source .pyWEenv/bin/activate";
+  public const string VirtualEnvironmentPath = ".pyWEenv/bin/activate";
+
+  public const string LocalConfigPath = "workflow_examples/local_config.yaml";
+  
+  public const string StageFilePath = "workflow_examples/ipc/hutch_cwl.wfex.stage";
 
 }
