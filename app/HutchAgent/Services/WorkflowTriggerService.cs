336988608f974930909eeb315749897e500bd4d1
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-03-28 15:54:52 +0100

    add comments to WorkflowTriggerService.cs

--- app/HutchAgent/Services/WorkflowTriggerService.cs
+++ app/HutchAgent/Services/WorkflowTriggerService.cs
@@ -1,44 +1,52 @@
 using System.Diagnostics;
+using System.Text;
 using HutchAgent.Models;
 
 namespace HutchAgent.Services;
 
 public class WorkflowTriggerService
 {
+  /// <summary>
+  /// Install and run WfExS given 
+  /// </summary>
+  /// <exception cref="Exception"></exception>
   public void TriggerWfexs(){
     
     const string cmd = "bash";
-    const string args = "";
-    const string activateVenv = "source" + WorkflowTriggerOptions.VirtualEnvironmentPath ;
-    var commandsToExecute = new List<string>(){
-      "pip install --upgrade pip wheel",
-      "pip install -r requirements.txt",
-      "pip install -r dev-requirements.txt",
-      "pip install -r mypy-requirements.txt",
-      "python /path/to/script arg1 arg2 arg3"
+    const string executionArgs = "";
+    const string activateVenv = "source " + WorkflowTriggerOptions.VirtualEnvironmentPath;
+    // Commands to install WfExS and execute a workflow
+    // given a path to the local config file and a path to the stage file of a workflow
+    var commands = new List<string>()
+    {
+      "apt-get install python3-pip",
+      "yes | ./full-installer.bash",
+      "yes | ./singularity-local-installer.bash",
+      $"./WfExS-backend.py  -L {WorkflowTriggerOptions.LocalConfigPath} execute -W {WorkflowTriggerOptions.StageFilePath}"
     };
-
-    var startInfo = new ProcessStartInfo
+    
+    var processStartInfo = new ProcessStartInfo
     {
       RedirectStandardOutput = true,
       RedirectStandardInput = true,
       RedirectStandardError = true,
       UseShellExecute = false,
       CreateNoWindow = true,
-      Arguments = args,
+      Arguments = executionArgs,
       FileName = cmd,
       WorkingDirectory = WorkflowTriggerOptions.ExecutorPath
     };
 
-    var process = Process.Start(startInfo);
+    var process = Process.Start(processStartInfo);
     if (process == null)
       throw new Exception("Could not start process");
 
     using var streamWriter = process.StandardInput;
     if (streamWriter.BaseStream.CanWrite)
     {
+      // activate python virtual environment
       streamWriter.WriteLine(activateVenv);
-      foreach (var command in commandsToExecute)
+      foreach (var command in commands)
       {
         streamWriter.WriteLine(command);
       }
