1fd5a8e031993400be39bb7914fec9040c510650
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-03-28 15:53:54 +0100

    add dependency injection to Program.cs

--- app/HutchAgent/Program.cs
+++ app/HutchAgent/Program.cs
@@ -1,55 +1,15 @@
-﻿using System.Diagnostics;
-using HutchAgent.Models;
-using HutchAgent.Services;
+﻿using HutchAgent.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HutchAgent;
-class Program
-{  
-    // private static readonly WorkflowTriggerService _workflowTriggerService;
-   
-    static void Main(string[] args)
+public class Program
+  {
+    public static void Main(string[] args)
     {
-      string directory = Directory.GetCurrentDirectory();
-      Console.WriteLine(directory);
-      const string cmd = "bash";
-      const string executionArgs = "";
-      const string activateVenv = "source" + WorkflowTriggerOptions.VirtualEnvironmentPath ;
-      var commandsToExecute = new List<string>(){
-        "pip install --upgrade pip wheel",
-        "pip install -r requirements.txt",
-        "pip install -r dev-requirements.txt",
-        "pip install -r mypy-requirements.txt",
-        "./full-installer.bash",
-        "python WfExS-backend.py --full-help"
-      };
-      
-      var startInfo = new ProcessStartInfo
-      {
-        RedirectStandardOutput = true,
-        RedirectStandardInput = true,
-        RedirectStandardError = true,
-        UseShellExecute = false,
-        CreateNoWindow = true,
-        Arguments = executionArgs,
-        FileName = cmd,
-        WorkingDirectory = WorkflowTriggerOptions.ExecutorPath
-      };
-
-      var process = Process.Start(startInfo);
-      if (process == null)
-        throw new Exception("Could not start process");
-
-      using var streamWriter = process.StandardInput;
-      if (streamWriter.BaseStream.CanWrite)
-      {
-        streamWriter.WriteLine(activateVenv);
-        foreach (var command in commandsToExecute)
-        {
-          streamWriter.WriteLine(command);
-        }
-        streamWriter.Flush();
-        streamWriter.Close();
-      }
+      var serviceProvider = new ServiceCollection()
+        .AddLogging()
+        .AddTransient<WorkflowTriggerService>()
+        .BuildServiceProvider();
+      new WorkflowTriggerService().TriggerWfexs();
     }
-    
-}
+  }
