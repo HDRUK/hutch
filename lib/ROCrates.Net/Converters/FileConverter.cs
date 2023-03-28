0bd520e659f0afd9662e572922acc6c6ae41b9fa
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-03-28 09:46:47 +0100

    file converter Read

--- lib/ROCrates.Net/Converters/FileConverter.cs
+++ lib/ROCrates.Net/Converters/FileConverter.cs
@@ -1,6 +1,19 @@
+using System.Text.Json;
+using File = ROCrates.Models.File;
+
 namespace ROCrates.Converters;
 
 public class FileConverter : EntityConverter
 {
-  // Todo: override `Read` to return a File
+  public override File? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    var properties = _parseJson(ref reader);
+
+    // Object is invalid if there is no @id or @type key
+    if (Id is null || Type is null)
+      throw new InvalidDataException("Either one of, or both @id and @type are not in the JSON.");
+
+    var file = new File(identifier: Id, properties: properties);
+    return file;
+  }
 }
