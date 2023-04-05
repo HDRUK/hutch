6eadbf184f7b7472249b5ae11cad2e941e3f414c
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2023-04-05 15:31:01 +0100

    AB#97882: HutchAgent to execute wfexs (#175)

--- app/HutchAgent/Program.cs
+++ app/HutchAgent/Program.cs
@@ -1,8 +1,21 @@
-﻿namespace HutchAgent;
-class Program
+﻿using HutchAgent.Models;
+using HutchAgent.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HutchAgent;
+
+public class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+  public static void Main(string[] args)
+  {
+    IHost host = Host.CreateDefaultBuilder(args)
+      .ConfigureServices((hostContext, services) =>
+      {
+        services.AddHostedService<WorkflowTriggerService>();
+        services.AddOptions<WorkflowTriggerOptions>().Bind(hostContext.Configuration.GetSection("Wfexs"));
+      })
+      .Build();
+    host.Run();
+  }
 }
---
+++ app/HutchAgent/Services/WorkflowTriggerService.cs
@@ -0,0 +1,90 @@
+using System.Diagnostics;
+using System.Text;
+using HutchAgent.Models;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HutchAgent.Services;
+
+public class WorkflowTriggerService : BackgroundService
+{
+  private readonly WorkflowTriggerOptions _workflowOptions;
+  private readonly ILogger<WorkflowTriggerService> _logger;
+
+  public WorkflowTriggerService(IOptions<WorkflowTriggerOptions> workflowOptions,
+    ILogger<WorkflowTriggerService> logger)
+  {
+    _logger = logger;
+    _workflowOptions = workflowOptions.Value;
+  }
+
+  protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    _logger.LogInformation(
+      "Executing Workflow with WfExS started.");
+
+    await TriggerWfexs();
+  }
+
+  /// <summary>
+  /// Install and run WfExS given 
+  /// </summary>
+  /// <exception cref="Exception"></exception>
+  public async Task TriggerWfexs()
+  {
+    const string cmd = "bash";
+    string activateVenv = "source " + _workflowOptions.VirtualEnvironmentPath;
+    // Commands to install WfExS and execute a workflow
+    // given a path to the local config file and a path to the stage file of a workflow
+    var commands = new List<string>()
+    {
+      $"./WfExS-backend.py  -L {_workflowOptions.LocalConfigPath} execute -W {_workflowOptions.StageFilePath}"
+    };
+
+    var processStartInfo = new ProcessStartInfo
+    {
+      RedirectStandardOutput = true,
+      RedirectStandardInput = true,
+      RedirectStandardError = true,
+      UseShellExecute = false,
+      CreateNoWindow = true,
+      FileName = cmd,
+      WorkingDirectory = _workflowOptions.ExecutorPath
+    };
+
+    // start process
+    var process = Process.Start(processStartInfo);
+    if (process == null)
+      throw new Exception("Could not start process");
+
+    using var streamWriter = process.StandardInput;
+    if (streamWriter.BaseStream.CanWrite)
+    {
+      // activate python virtual environment
+      await streamWriter.WriteLineAsync(activateVenv);
+      foreach (var command in commands)
+      {
+        await streamWriter.WriteLineAsync(command);
+      }
+
+      await streamWriter.FlushAsync();
+      streamWriter.Close();
+    }
+
+    var sb = new StringBuilder();
+    StreamReader reader = process.StandardOutput;
+    while (!process.HasExited)
+      sb.Append(await reader.ReadToEndAsync());
+    // end the process
+    process.Close();
+  }
+
+  public override async Task StopAsync(CancellationToken stoppingToken)
+  {
+    _logger.LogInformation(
+      "Hosted Service is stopping.");
+
+    await base.StopAsync(stoppingToken);
+  }
+}
