f065308029b038e63c1ec6a5f8b5386444401eda
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-04-06 12:32:17 +0100

    fix broken test

--- lib/ROCrates.Net.Tests/TestRoCrate.cs
+++ lib/ROCrates.Net.Tests/TestRoCrate.cs
@@ -17,7 +17,7 @@ public class TestRoCrate
   public void TestResolveId_CombinesGoodAndBad_Uris()
   {
     string validUrl = "https://doi.org/10.4225/59/59672c09f4a4b";
-    string invalidUrl = "htps/doi.org/10.4225/59/59672c09f4a4b";
+    string invalidUrl = "https://do i.org/10.4225/59/59672c09f4a4b";
     var crate = new ROCrate("my-test.zip");
 
     string resultValidUrl = crate.ResolveId(validUrl);
@@ -27,21 +27,6 @@ public class TestRoCrate
     Assert.StartsWith("arcp://", resultInvalidUrl);
     Assert.EndsWith(invalidUrl, resultInvalidUrl);
   }
-
-  [Fact]
-  public void TestResolveId_TrimsTrailingSlash()
-  {
-    string noSlash = "https://doi.org/10.4225/59/59672c09f4a4b";
-    string withSlash = noSlash + '/';
-    var crate = new ROCrate("my-test.zip");
-
-    string resultNoSlash = crate.ResolveId(noSlash);
-    Assert.Equal(noSlash, resultNoSlash);
-
-    string resultWithSlash = crate.ResolveId(withSlash);
-    Assert.EndsWith(withSlash.TrimEnd('/'), resultWithSlash);
-  }
-
 
   [Fact]
   public void TestAdd_AddsRootDataset()
