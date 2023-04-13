bd146781fe1427a99bd65b923dc8d9b88b354d71
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-04-13 16:16:38 +0100

    try running the Write function in a test

---
+++ lib/ROCrates.Net.Tests/TestPreview.cs
@@ -0,0 +1,31 @@
+using System.Text.Json.Nodes;
+using ROCrates.Models;
+
+namespace ROCrates.Tests;
+
+public class TestPreview
+{
+  private ROCrate _roCrate = new();
+
+  [Fact]
+  public void TestWrite_Creates_PreviewFile()
+  {
+    // Arrange
+    var rootJson = System.IO.File.ReadAllText("Fixtures/test-root-dataset.json").TrimEnd();
+    var rootProperties = JsonNode.Parse(rootJson).AsObject();
+    var rootDataset = new RootDataset(_roCrate, properties: rootProperties);
+
+    var previewJson = System.IO.File.ReadAllText("Fixtures/test-preview.json").TrimEnd();
+    var previewProperties = JsonNode.Parse(previewJson).AsObject();
+    var preview = new Preview(_roCrate, properties: previewProperties);
+
+    var previewBasePath = "./";
+    var previewFileName = Path.Combine(previewBasePath, "ro-crate-preview.html");
+    _roCrate.Add(rootDataset, preview);
+
+    // Act
+    preview.Write(previewJson);
+
+    // Assert
+  }
+}
