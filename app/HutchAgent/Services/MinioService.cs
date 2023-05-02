3fa255cf40b2f6da8af548d337376b960b73cd37
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-05-02 11:37:48 +0100

    upload file method in MinioService.cs

--- app/HutchAgent/Services/MinioService.cs
+++ app/HutchAgent/Services/MinioService.cs
@@ -1,6 +1,7 @@
 using HutchAgent.Config;
 using Microsoft.Extensions.Options;
 using Minio;
+using Minio.Exceptions;
 
 namespace HutchAgent.Services;
 
@@ -15,5 +16,33 @@ public class MinioService
       .WithCredentials(minioOptions.Value.AccessKey, minioOptions.Value.SecretKey)
       .WithSSL(minioOptions.Value.Secure)
       .Build();
+  }
+
+  /// <summary>
+  /// Upload a file to an S3 bucket.
+  /// </summary>
+  /// <param name="bucketName">The name of the bucket to which the file will be uploaded.</param>
+  /// <param name="filePath">The path of the file to be uploaded.</param>
+  /// <exception cref="BucketNotFoundException">Thrown when the given bucket doesn't exists.</exception>
+  /// <exception cref="MinioException">Thrown when any other error related to MinIO occurs.</exception>
+  /// <exception cref="FileNotFoundException">Thrown when the file to be uploaded does not exist.</exception>
+  public async Task UploadToBucket(string bucketName, string filePath)
+  {
+    var exists = await _bucketExists(bucketName);
+    if (!exists) throw new BucketNotFoundException(bucketName, $"No such bucket: {bucketName}");
+
+    if (!System.IO.File.Exists(filePath)) throw new FileNotFoundException();
+
+    var putObjectArgs = new PutObjectArgs()
+      .WithBucket(bucketName)
+      .WithFileName(filePath)
+      .WithObject(System.IO.Path.GetFileName(filePath));
+    await _minioClient.PutObjectAsync(putObjectArgs);
+  }
+
+  private async Task<bool> _bucketExists(string bucketName)
+  {
+    var args = new BucketExistsArgs().WithBucket(bucketName);
+    return await _minioClient.BucketExistsAsync(args);
   }
 }
