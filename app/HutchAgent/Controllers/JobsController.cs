55b9f262330a65e9e8561a45159ed0a2f7c438ed
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-05-02 16:52:51 +0100

    Create JobsController.cs

---
+++ app/HutchAgent/Controllers/JobsController.cs
@@ -0,0 +1,38 @@
+using HutchAgent.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HutchAgent.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+[Route("api/[controller]")]
+public class JobsController : ControllerBase
+{
+  readonly WorkflowTriggerService _workflowTriggerService;
+
+  public JobsController(WorkflowTriggerService workflowTriggerService)
+  {
+    _workflowTriggerService = workflowTriggerService;
+  }
+
+  [HttpPost]
+  public async Task<IActionResult> Unpack(IFormFile? job)
+  {
+    if (job == null)
+      return BadRequest();
+    try
+    {
+      using var sr = job.OpenReadStream();
+
+      {
+        await _workflowTriggerService.TriggerWfexs(sr);
+        return Accepted();
+      }
+    }
+    catch
+    {
+      return BadRequest();
+    }
+  }
+}
