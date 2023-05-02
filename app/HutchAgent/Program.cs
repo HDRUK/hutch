76b27ba9dec3221802444519ffb505da0a379f91
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-05-02 10:34:33 +0100

    add MinIO options to Program.cs

--- app/HutchAgent/Program.cs
+++ app/HutchAgent/Program.cs
@@ -1,4 +1,14 @@
+using HutchAgent.Config;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region Configure Service
+
+builder.Services
+  .Configure<MinioOptions>(builder.Configuration.GetSection("MinIO"));
+
+#endregion
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
