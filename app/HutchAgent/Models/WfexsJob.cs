f9f18f43b148afd22171d282766b12226a849efe
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-05-09 15:58:30 +0100

    wfexs job class

---
+++ app/HutchAgent/Models/WfexsJob.cs
@@ -0,0 +1,13 @@
+namespace HutchAgent.Models;
+
+/// <summary>
+/// Represents a run of WfExS. Contains the path to where the RO-Crate should be unpacked, the ID of the WfExS run
+/// and whether or not the WfExS run has been completed.
+/// </summary>
+public class WfexsJob
+{
+  public int Id { get; set; }
+  public string UnpackedPath { get; set; } = string.Empty;
+  public string WfexsRunId { get; set; } = string.Empty;
+  public bool RunFinished { get; set; }
+}
