027f113da3bb84339ff7f1d27fea57886e4b614f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-05-15 16:08:04 +0100

    merge changes fixing scope problem

--- app/HutchAgent/Services/WatchFolderService.cs
+++ app/HutchAgent/Services/WatchFolderService.cs
@@ -8,18 +8,16 @@ public class WatchFolderService : BackgroundService
 {
   private readonly WatchFolderOptions _options;
   private readonly ILogger<WatchFolderService> _logger;
-  private readonly MinioService _minioService;
-  private readonly WfexsJobService _wfexsJobService;
-  private readonly CrateMergerService _crateMergerService;
+  private MinioService? _minioService;
+  private WfexsJobService? _wfexsJobService;
+  private CrateMergerService? _crateMergerService;
   private readonly IServiceProvider _serviceProvider;
 
-  public WatchFolderService(IOptions<WatchFolderOptions> options, ILogger<WatchFolderService> logger, IServiceProvider serviceProvider)
+  public WatchFolderService(IOptions<WatchFolderOptions> options, ILogger<WatchFolderService> logger,
+    IServiceProvider serviceProvider)
   {
     _options = options.Value;
     _logger = logger;
-    _minioService = serviceProvider.GetService<MinioService>() ?? throw new InvalidOperationException();
-    _wfexsJobService = serviceProvider.GetService<WfexsJobService>() ?? throw new InvalidOperationException();
-    _crateMergerService = serviceProvider.GetService<CrateMergerService>() ?? throw new InvalidOperationException();
     _serviceProvider = serviceProvider;
   }
 
@@ -33,9 +31,12 @@ public class WatchFolderService : BackgroundService
 
     while (!stoppingToken.IsCancellationRequested)
     {
-      using (var scope = _serviceProvider.CreateScope()) {
+      using (var scope = _serviceProvider.CreateScope())
+      {
         _minioService = scope.ServiceProvider.GetService<MinioService>() ?? throw new InvalidOperationException();
         _wfexsJobService = scope.ServiceProvider.GetService<WfexsJobService>() ?? throw new InvalidOperationException();
+        _crateMergerService = scope.ServiceProvider.GetService<CrateMergerService>() ??
+                              throw new InvalidOperationException();
         _watchFolder();
         MergeResults();
       }
