56615c0678a5b0069e89a33597345d73b32684b8
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-05-16 10:02:06 +0100

    Commit migrations

---
+++ app/HutchAgent/Migrations/20230512153934_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HutchAgent.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WfexsJobs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UnpackedPath = table.Column<string>(type: "TEXT", nullable: false),
+                    WfexsRunId = table.Column<string>(type: "TEXT", nullable: false),
+                    RunFinished = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WfexsJobs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WfexsJobs");
+        }
+    }
+}
