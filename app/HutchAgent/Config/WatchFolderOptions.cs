21db43be921887e1b3b9133ff398f38a1a3d9cfb
Vasiliki Panagi <75254435+vpnu@users.noreply.github.com>
2023-05-18 13:32:20 +0100

    AB#104721:Change polling interval from double to int (#196)

--- app/HutchAgent/Config/WatchFolderOptions.cs
+++ app/HutchAgent/Config/WatchFolderOptions.cs
@@ -3,5 +3,5 @@ namespace HutchAgent.Config;
 public class WatchFolderOptions
 {
   public string Path { get; set; } = string.Empty;
-  public double PollingIntervalSeconds { get; set; } = 5.0;
+  public int PollingIntervalSeconds { get; set; } = 5;
 }
