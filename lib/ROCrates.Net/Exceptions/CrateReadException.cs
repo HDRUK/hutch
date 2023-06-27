ce818a977c6888ea7742bc6897b53f88a25eae68
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-06-27 16:04:12 +0100

    crate read exception

---
+++ lib/ROCrates.Net/Exceptions/CrateReadException.cs
@@ -0,0 +1,19 @@
+namespace ROCrates.Exceptions;
+
+/// <summary>
+/// An exception that can be thrown whenever an RO-crate cannot be read.
+/// </summary>
+public class CrateReadException : Exception
+{
+  public CrateReadException()
+  {
+  }
+
+  public CrateReadException(string? message, Exception? innerException) : base(message, innerException)
+  {
+  }
+
+  public CrateReadException(string? message) : base(message)
+  {
+  }
+}
