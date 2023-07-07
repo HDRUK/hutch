c1d1bbc49750eebc4b1331cbe3b3e6a1ccca03b1
Shason <s.gurung@nottingham.ac.uk>
2023-07-07 10:27:53 +0100

    AB#108982: Results Merging amendments (#217)
    Co-authored-by: Daniel Lea <dan.carr.lea@gmail.com>

--- app/HutchAgent/Services/CrateMergerService.cs
+++ app/HutchAgent/Services/CrateMergerService.cs
@@ -8,6 +8,7 @@ namespace HutchAgent.Services;
 /// </summary>
 public class CrateMergerService
 {
+  private readonly string _pathToOutputDir = Path.Combine("data","outputs");
   /// <summary>
   /// Extract a source zipped RO-Crate into an unzipped destination RO-Crate `Data/outputs` directory and zip the
   /// destination RO-Crate.
@@ -25,7 +26,7 @@ public class CrateMergerService
     if (!destinationInfo.Exists) throw new DirectoryNotFoundException($"{mergeInto} does not exist.");
 
     // Create output directory `Data/outputs/` to extract execution crate
-    var outputDir = Path.Combine(mergeInto, "Data", "outputs");
+    var outputDir = Path.Combine(mergeInto, _pathToOutputDir);
     Directory.CreateDirectory(outputDir);
     
     // Extract the result (RO-Crate) into the unzipped original RO-Crate
@@ -53,19 +54,22 @@ public class CrateMergerService
   /// Update the target metadata file in an RO-Crate.
   /// </summary>
   /// <param name="pathToMetadata">The path to the metadata file that needs updating.</param>
-  /// <param name="fileToAdd">The path of the file to add to the metadata.</param>
   /// <exception cref="FileNotFoundException">
-  /// Metadata file and/or file to be added to the metadata could not be found.
+  /// Metadata file could not be found.
   /// </exception>
   /// <exception cref="InvalidDataException">The metadata file is invalid.</exception>
-  public void UpdateMetadata(string pathToMetadata, string fileToAdd)
+  public void UpdateMetadata(string pathToMetadata)
   {
     if (!File.Exists(pathToMetadata))
       throw new FileNotFoundException("Could not locate the metadata for the RO-Crate.");
 
-    if (!File.Exists(fileToAdd))
-      throw new FileNotFoundException("Could not locate the file to add to the metadata.");
-
+    var rootDirInfo = new DirectoryInfo(pathToMetadata).Parent; // get root dir info
+    
+    var folderToAdd = Path.Combine(rootDirInfo.FullName, _pathToOutputDir);
+    
+    if (!Directory.Exists(folderToAdd))
+      throw new DirectoryNotFoundException("Could not locate the folder to add to the metadata.");
+    
     var metadataJson = File.ReadAllText(pathToMetadata);
     var metadata = JsonNode.Parse(metadataJson);
     if (metadata is null) throw new InvalidDataException($"{pathToMetadata} is not a valid JSON file.");
@@ -74,9 +78,18 @@ public class CrateMergerService
       throw new InvalidDataException("Cannot find entities in the RO-Crate metadata.");
 
     var rootDatasetProperties = graph.AsArray().First(g => g["@id"].ToString() == "./");
-    var file = new ROCrates.Models.File(source: Path.GetFileName(fileToAdd)).Serialize();
-    rootDatasetProperties["hasPart"].AsArray().Add(JsonNode.Parse(file));
+    var folder = new ROCrates.Models.Dataset(source: Path.GetRelativePath(rootDirInfo.FullName, folderToAdd)).Serialize();
+    rootDatasetProperties["hasPart"].AsArray().Add(JsonNode.Parse(folder));
 
     File.WriteAllText(pathToMetadata, metadata.ToString());
+  }
+  
+  /// <summary>
+  /// Delete container images
+  /// </summary>
+  /// <param name="pathToImagesDir">The path to container images directory.</param>
+  public void DeleteContainerImages(string pathToImagesDir)
+  {
+    Directory.Delete(pathToImagesDir, recursive: true); // TODO
   }
 }
