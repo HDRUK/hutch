8f49aa921560ab039978254796e520e675b24361
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-08-10 15:48:01 +0100

    put back just exit code
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/HutchAgent/Data/Entities/WfexsJob.cs
+++ app/HutchAgent/Data/Entities/WfexsJob.cs
@@ -11,6 +11,5 @@ public class WfexsJob
   public string WfexsRunId { get; set; } = string.Empty;
   public bool RunFinished { get; set; }
   public int Pid { get; set; }
-  public string ResultsPath { get; set; } = string.Empty;
   public int ExitCode { get; set; }
 }
---
+++ app/HutchAgent/Migrations/20230810144733_AddExitCode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HutchAgent.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExitCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExitCode",
+                table: "WfexsJobs",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExitCode",
+                table: "WfexsJobs");
+        }
+    }
+}
