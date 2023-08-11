7629b49b6449130f21242e8e5736d7559e1d4a6c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-08-11 16:31:02 +0100

    tidy up log statements
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/HutchAgent/HostedServices/JobPollingHostedService.cs
+++ app/HutchAgent/HostedServices/JobPollingHostedService.cs
@@ -80,8 +80,7 @@ public class JobPollingHostedService : BackgroundService
     foreach (var job in finishedJobs)
     {
       var pathToUpload = Path.Combine(
-        _workflowTriggerOptions.ExecutorPath,
-        "wfexs-backend-test_WorkDir",
+        _workDir,
         job.WfexsRunId,
         "outputs",
         "execution.crate.zip");
@@ -97,23 +96,24 @@ public class JobPollingHostedService : BackgroundService
       if (_resultsStoreWriter is null) throw new NullReferenceException("_resultsStoreWriter instance not available");
       if (await _resultsStoreWriter.ResultExists(pathToUploadInfo.Name))
       {
-        _logger.LogInformation($"{pathToUploadInfo.Name} already exists in S3.");
+        _logger.LogInformation("{Name} already exists in S3.", pathToUploadInfo.Name);
         continue;
       }
 
       try
       {
-        _logger.LogInformation($"Attempting to upload {pathToUploadInfo.Name} to S3.");
+        _logger.LogInformation("Attempting to upload {Name} to S3.", pathToUploadInfo.Name);
         await _resultsStoreWriter.WriteToStore(pathToUploadInfo.FullName);
-        _logger.LogInformation($"Successfully uploaded {pathToUploadInfo.Name} to S3.");
+        _logger.LogInformation("Successfully uploaded {Name} to S3.", pathToUploadInfo.Name);
       }
       catch (BucketNotFoundException)
       {
-        _logger.LogCritical($"Unable to upload {pathToUploadInfo.Name} to S3. The configured bucket does not exist.");
+        _logger.LogCritical("Unable to upload {Name} to S3. The configured bucket does not exist.",
+          pathToUploadInfo.Name);
       }
       catch (MinioException)
       {
-        _logger.LogError($"Unable to upload {pathToUploadInfo.Name} to S3. An error occurred with the S3 server.");
+        _logger.LogError("Unable to upload {Name} to S3. An error occurred with the S3 server.", pathToUploadInfo.Name);
       }
     }
   }
@@ -125,8 +125,7 @@ public class JobPollingHostedService : BackgroundService
     foreach (var job in finishedJobs)
     {
       var jobWorkDir = Path.Combine(
-        _workflowTriggerOptions.ExecutorPath,
-        "wfexs-backend-test_WorkDir",
+        _workDir,
         job.WfexsRunId);
       var sourceZip = Path.Combine(jobWorkDir, "outputs", $"{job.WfexsRunId}.zip");
       var pathToMetadata = Path.Combine(job.UnpackedPath); // directory path to ro-crate-metadata.json
@@ -137,7 +136,7 @@ public class JobPollingHostedService : BackgroundService
 
       if (!File.Exists(sourceZip))
       {
-        _logger.LogError($"Could not locate {sourceZip}.");
+        _logger.LogError("Could not locate {sourceZip}.", sourceZip);
         continue;
       }
 
@@ -150,7 +149,7 @@ public class JobPollingHostedService : BackgroundService
       if (_resultsStoreWriter is null) throw new NullReferenceException("_resultsStoreWriter instance not available");
       if (await _resultsStoreWriter.ResultExists(mergedZip))
       {
-        _logger.LogInformation($"Merged Crate {mergedZip} already exists in results store.");
+        _logger.LogInformation("Merged Crate {mergedZip} already exists in results store.", mergedZip);
         continue;
       }
 
