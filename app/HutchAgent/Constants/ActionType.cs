9f7ba418e3c6669b5ba0295f627c4cf008f73ad0
vpnu <vasiliki.panagi@nottingham.ac.uk>
2023-08-11 16:08:25 +0100

    Create ActionType.cs based on AssessAction additionalType

---
+++ app/HutchAgent/Constants/ActionType.cs
@@ -0,0 +1,17 @@
+namespace HutchAgent.Constants;
+/// <summary>
+/// Using terms from the Safe Haven Provenance (SHP) ontology
+/// </summary>
+public static class ActionType
+{
+  public const string CheckValueType = "https://w3id.org/shp#CheckValue";
+
+  public const string ValidationCheck = "https://w3id.org/shp#ValidationCheck";
+
+  public const string SignOff = "https://w3id.org/shp#SignOff";
+
+  public const string DisclosureCheck = "https://w3id.org/shp#DisclosureCheck";
+
+  public const string GenerateCheckValue = "https://w3id.org/shp#GenerateCheckValue";
+  
+}
