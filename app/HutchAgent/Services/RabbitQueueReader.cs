18e8f1e9fb6f69a475f316cdb1c76449028fdb40
Jon Couldridge <jcouldridge@outlook.com>
2023-08-31 15:57:42 +0100

    RabbitMQ QueueReader can pop messages

---
+++ app/HutchAgent/Services/RabbitQueueReader.cs
@@ -0,0 +1,45 @@
+using System.Text;
+using System.Text.Json;
+using HutchAgent.Config;
+using HutchAgent.Models;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+
+namespace HutchAgent.Services;
+
+public class RabbitQueueReader : IQueueReader
+{
+  private readonly RabbitQueueOptions _queueOptions;
+  private readonly IConnection _connection;
+  private readonly IModel _channel;
+
+  public RabbitQueueReader(IOptions<RabbitQueueOptions> queueOptions)
+  {
+    _queueOptions = queueOptions.Value;
+    var connectionFactory = new ConnectionFactory()
+    {
+      HostName = _queueOptions.HostName,
+      Port = _queueOptions.Port,
+      UserName = _queueOptions.UserName,
+      Password = _queueOptions.Password
+    };
+    _connection = connectionFactory.CreateConnection();
+    _channel = _connection.CreateModel();
+  }
+
+  /// <inheritdoc/>
+  public JobQueueMessage? Pop(string queueName)
+  {
+    _channel.QueueDeclare(queue: queueName,
+      durable: false,
+      exclusive: false,
+      autoDelete: false,
+      arguments: null);
+
+    var result = _channel.BasicGet(queueName, autoAck: true);
+    if(result is null) return null;
+
+    var body = Encoding.UTF8.GetString(result.Body.ToArray());
+    return JsonSerializer.Deserialize<JobQueueMessage>(body);
+  }
+}
