b4e7a34b4f869c20df256255568a2b0423fa6dc0
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-09-04 14:58:25 +0100

    add job service
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/HutchAgent/Services/FinalisationService.cs
+++ app/HutchAgent/Services/FinalisationService.cs
@@ -6,16 +6,18 @@ public class FinalisationService
   private readonly CrateService _crateService;
   private readonly ILogger<FinalisationService> _logger;
   private readonly IResultsStoreWriter _storeWriter;
+  private readonly WorkflowJobService _jobService;
 
   public FinalisationService(
     BagItService bagItService,
     CrateService crateService,
     ILogger<FinalisationService> logger,
-    IResultsStoreWriter storeWriter)
+    IResultsStoreWriter storeWriter, WorkflowJobService jobService)
   {
     _bagItService = bagItService;
     _crateService = crateService;
     _logger = logger;
     _storeWriter = storeWriter;
+    _jobService = jobService;
   }
 }
