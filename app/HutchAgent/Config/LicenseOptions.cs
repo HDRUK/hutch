717fe591f4705a6b4d3b311802272ee46b0e7ab5
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-09-07 14:20:52 +0100

    license options
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

---
+++ app/HutchAgent/Config/LicenseOptions.cs
@@ -0,0 +1,10 @@
+namespace HutchAgent.Config;
+
+public class LicenseOptions
+{
+  public string Uri { get; set; } = "https://spdx.org/licenses/CC-BY-4.0";
+
+  public string Name { get; set; } = "Creative Commons Attribution 4.0 International";
+
+  public string Identifier { get; set; } = "CC-BY-4.0";
+}
