cd2b78a2a4cd2349a7930d2842f5ced7fab41389
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-09-08 14:43:02 +0100

    use JsonObject to represent other license properties
    Signed-off-by: Daniel Lea <Daniel.Lea@nottingham.ac.uk>

--- app/HutchAgent/Config/LicenseOptions.cs
+++ app/HutchAgent/Config/LicenseOptions.cs
@@ -1,10 +1,16 @@
+using System.Text.Json.Nodes;
+
 namespace HutchAgent.Config;
 
 public class LicenseOptions
 {
+  /// <summary>
+  /// The @id of the license entity to add to the outputs.
+  /// </summary>
   public string Uri { get; set; } = "https://spdx.org/licenses/CC-BY-4.0";
 
-  public string Name { get; set; } = "Creative Commons Attribution 4.0 International";
-
-  public string Identifier { get; set; } = "CC-BY-4.0";
+  /// <summary>
+  /// This attribute contains any other properties for the license entity.
+  /// </summary>
+  public JsonObject? Properties { get; set; } = null;
 }
