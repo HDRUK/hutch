72100475f8afa74642f69729ec16c606d086493d
Jon Couldridge <jcouldridge@outlook.com>
2023-09-21 15:53:36 +0100

    added post-refactor initial migration for local status db

---
+++ app/HutchAgent/Data/Migrations/20230921145246_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HutchAgent.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkflowJobs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    ProjectName = table.Column<string>(type: "TEXT", nullable: false),
+                    ProjectId = table.Column<string>(type: "TEXT", nullable: false),
+                    OutputUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    OutputAccess = table.Column<string>(type: "TEXT", nullable: true),
+                    DataAccess = table.Column<string>(type: "TEXT", nullable: true),
+                    WorkingDirectory = table.Column<string>(type: "TEXT", nullable: false),
+                    ExecutorRunId = table.Column<string>(type: "TEXT", nullable: false),
+                    ExitCode = table.Column<int>(type: "INTEGER", nullable: true),
+                    ExecutionStartTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    EndTime = table.Column<DateTimeOffset>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkflowJobs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkflowJobs");
+        }
+    }
+}
