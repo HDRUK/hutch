ed7e54954168ff787f2e37cc325d3e2f47616fe8
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2023-10-06 10:55:26 +0100

    AB#130446: Standalone swagger in docs website (#256)

---
+++ website/src/pages/swagger.tsx
@@ -0,0 +1,12 @@
+import Layout from "@theme/Layout";
+import React from "react";
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
+
+const SwaggerPage = () => (
+  <Layout>
+    <SwaggerUI url="/hutch/swagger.json" supportedSubmitMethods={[]} />
+  </Layout>
+);
+
+export default SwaggerPage;
