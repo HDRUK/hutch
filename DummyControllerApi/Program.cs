dca6aad72506e7b60763d050b86832446529e212
Jon Couldridge <jcouldridge@outlook.com>
2023-10-12 17:18:54 +0100

    added jwt bearer auth to controller api

--- DummyControllerApi/Program.cs
+++ DummyControllerApi/Program.cs
@@ -1,16 +1,41 @@
 using DummyControllerApi.Config;
 using DummyControllerApi.Services;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Auth
+builder.Services.AddAuthentication()
+  .AddJwtBearer(
+    opts =>
+    {
+      opts.TokenValidationParameters = new TokenValidationParameters
+      {
+        // TODO we're not interested in actually checking in with whatever idp is in use
+        // we just want to confirm Hutch is sending a token
+        // everything else will be environment setup dependent anyway
+        ValidateIssuer = false,
+        ValidateIssuerSigningKey = false,
+        RequireExpirationTime = true
+      };
+    });
+builder.Services.AddAuthorization(o =>
+{
+  //o.DefaultPolicy = ;
+});
+
+// Configure Options Models
 builder.Services.Configure<EgressBucketDetailsOptions>(builder.Configuration.GetSection("EgressBucketDetails"));
 
+// MVC and stuff
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Application Services
 
 // Use this combo for delaying approvals while running
 builder.Services
@@ -28,8 +53,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app
+  .UseAuthentication()
+  .UseAuthorization();
 
-app.MapControllers();
+app.MapControllers().RequireAuthorization();
 
 app.Run();
