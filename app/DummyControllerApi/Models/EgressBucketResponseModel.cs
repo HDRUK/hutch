78344ee3feb6c3707155a75da76c731bc5b69464
Jon Couldridge <jcouldridge@outlook.com>
2023-10-19 11:05:36 +0100

    updated dummy TRE Controller API  to match agreed api changes

--- app/DummyControllerApi/Models/EgressBucketResponseModel.cs
+++ app/DummyControllerApi/Models/EgressBucketResponseModel.cs
@@ -2,8 +2,26 @@ namespace DummyControllerApi.Models;
 
 public class EgressBucketResponseModel
 {
+  /// <summary>
+  /// Submission ID
+  /// </summary>
+  public string SubId { get; set; } = string.Empty;
+  
+  /// <summary>
+  /// The Minio Host details
+  /// </summary>
   public string Host { get; set; } = string.Empty;
+  
+  /// <summary>
+  /// The name of the target Minio bucket
+  /// </summary>
   public string Bucket { get; set; } = string.Empty;
+  
+  /// <summary>
+  /// A path to prefix to object id's uploaded for this submission
+  /// </summary>
+
+  public string Path { get; set; } = string.Empty;
   
   /// <summary>
   /// A real TRE Controller API is unlikely to provide this (even though Hutch will accept it)
